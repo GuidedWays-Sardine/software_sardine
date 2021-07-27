081b3ff98209ca10255721a4a4ca973765c55f3c
Mathieu DENGLOS <matdenglos@gmail.com>
2021-07-27 15:56:38 +0200

    add signal for button "lancer"

--- src/initialisation/signals/bottom_buttons.py
+++ src/initialisation/signals/bottom_buttons.py
@@ -18,6 +18,8 @@ class BottomButtons:
         """
         # Boutons quitter/lancer (obligatoires pour le lancement de l'application)
         application.win.findChild(QObject, "quitter").clicked.connect(lambda: self.on_quit_clicked(application))
+        application.win.findChild(QObject, "lancer").clicked.connect(lambda: self.on_launch_clicked(application))
+
 
     def on_quit_clicked(self, application):
         """Ferme la fenêtre d'initialisation
@@ -27,4 +29,15 @@ class BottomButtons:
         application: `InitialisationWindow`
             L'instance source de l'application d'initialisation, pour les widgets
         """
+        application.app.quit()
+
+    def on_launch_clicked(self, application):
+        """Ferme la fenêtre d'initialisation et indique au programme de récupérer les données entrées
+
+        Parameters
+        ----------
+        application: `InitialisationWindow`
+            L'instance source de l'application d'initialisation, pour les widgets"""
+        # Indique que le simulateur va être lancée et ferme l'application
+        application.launch_simulator = True
         application.app.quit()
