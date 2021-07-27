29a340a2cf9ca28d5804d2ba3a324e8cc4255901
Mathieu DENGLOS <matdenglos@gmail.com>
2021-07-27 14:30:05 +0200

    add exception handling in case of missing or incorrect .qml file

--- src/initialisation/initialisation_window.py
+++ src/initialisation/initialisation_window.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 
 from PyQt5.QtWidgets import QApplication
@@ -12,12 +13,26 @@ class InitialisationWindow:
     win = None
 
     def __init__(self):
-        """initialise toutes les fenêtre du programme d'initialisation du simulateur sardine"""
+        """initialise toutes les fenêtre du programme d'initialisation du simulateur sardine
+
+        Raises
+        ------
+        FileNotFoundError
+            Soulevé quand le fichier .qml de la fenêtre d'initialisation n'est pas trouvé
+        SyntaxError
+            Soulevé quand le fichier .qml de la fenêtre d'initialisation a une erreur de syntaxe et n'est pas lisible
+        """
         # Lance l'application et cherche pour le fichier QML avec tous les éléments de la fenêtre d'initialisation
         self.app = QApplication(sys.argv)
         self.app.setQuitOnLastWindowClosed(True)
         self.engine = QQmlApplicationEngine()
         self.engine.load('initialisation/initialisation_window.qml')
+
+        # Vérifie si le fichier qml de la fenêtre a bien été ouvert et compris, sinon jête une erreur
+        if not self.engine.rootObjects() and not os.path.isfile('initialisation/initialisation_window.qml'):
+            raise FileNotFoundError('Le fichier .qml pour la fenêtre d\'initialisation n\'a pas été trouvé')
+        elif not self.engine.rootObjects() and os.path.isfile('initialisation/initialisation_window.qml'):
+            raise SyntaxError('Le fichier .qml pour la fenêtre d\'initialisation contient des erreurs.')
 
         # Lance l'application
         self.win = self.engine.rootObjects()[0]
--- src/main.py
+++ src/main.py
@@ -8,8 +8,15 @@ def main():
     logging.info("Lancement de l'application d'initialisation du simulateur\n\n\n")
 
     # Lance le programme d'initialisation
-    app = ini.InitialisationWindow()
-
+    try:
+        app = ini.InitialisationWindow()
+    # Récupère une potentielle erreur fatale et la charge
+    except Exception as error:
+        logging.fatal('Erreur fatale lors du chargement de l\'application d\'initialisation simulateur\n\t\t' +
+                      'Erreur de type : ' + str(type(error)) + '\n\t\t' +
+                      'Avec comme message d\'erreur : ' + error.args[0] + '\n\n\t\t' +
+                      ''.join(traceback.format_tb(error.__traceback__)).replace('\n', '\n\t\t') + "\n")
+        exit(-1)
 
 if __name__ == "__main__":
     main()
