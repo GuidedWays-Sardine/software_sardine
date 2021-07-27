d6b51d499f7e344a8ed717c5e8a8375e9c0a96e6
Mathieu DENGLOS <matdenglos@gmail.com>
2021-07-27 16:06:32 +0200

    add necessary variables for the right buttons and the settings pages to function

--- src/initialisation/signals/right_buttons.py
+++ src/initialisation/signals/right_buttons.py
@@ -8,6 +8,15 @@ from PyQt5.QtCore import QObject
 
 class RightButtons:
 
+    #stoque les éléments de bases des boutons de droite
+    right_buttons = None
+    pages_stackview = None
+
+    # Stoque si la page est chargée et si elle est complète (pour lancer le simulateur
+    visible_pages = [None] * 8     # Stoque les pages que l'utilisateur peut afficher
+    is_fully_loaded = [False] * 8  # Stoque directement l'instance de la classe
+    is_completed = [False] * 8     # Détecte si la page est complété (égale à self.visible_pages si tout est complété)
+
     def __init__(self, application):
         """
         Permet d'initialiser les 8 boutons permanents (rb1 -> rb8) situés à droite de la fenêtre.
