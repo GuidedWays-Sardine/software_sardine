277dd614dbf4232bb584e3f3e19c0a80a36e6640
Mathieu DENGLOS <matdenglos@gmail.com>
2021-07-27 15:39:46 +0200

    Create signals file for the bottom and right buttons

---
+++ src/initialisation/signals/bottom_buttons.py
@@ -0,0 +1,12 @@
+import logging
+import os
+import traceback
+
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QObject
+
+
+class RightButtons:
+
+    def __init__(self):
+        logging.info("right buttons")
---
+++ src/initialisation/signals/right_buttons.py
@@ -0,0 +1,11 @@
+import logging
+import traceback
+
+from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QFileDialog
+
+
+class BottomButtons:
+
+    def __init__(self):
+        logging.info("bottom buttons")
