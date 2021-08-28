53c50f60487f55da6c3ed62b202370ea60d5a8d3
MathieuDenglos <matdenglos@gmail.com>
2021-08-28 23:30:33 +0200

    rework logic for page_rb1

--- src/initialisation/signals/page_rb/page_rb1.py
+++ src/initialisation/signals/page_rb/page_rb1.py
@@ -6,5 +6,13 @@ from PyQt5.QtCore import QObject
 
 class PageRB1:
 
-    def __init__(self, application, page, index):
-        logging.info("page 1 signals")
+    index = 1   # Attention dans les tableaux l'index comment à 0 donc index_tab = index - 1
+    page = None
+    current_button = None
+
+    def __init__(self, application, page, index, current_button):
+        self.index = index
+        self.page = page
+        self.current_button = current_button
+
+        application.is_completed[0] = True
