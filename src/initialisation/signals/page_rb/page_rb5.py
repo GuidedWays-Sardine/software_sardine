85766abef45eb74a6160ae28d2f2d64ab88ccaa7
MathieuDenglos <matdenglos@gmail.com>
2021-09-09 23:32:00 +0200

    add screen size and amount initialisation for graphic section

--- src/initialisation/signals/page_rb/page_rb5.py
+++ src/initialisation/signals/page_rb/page_rb5.py
@@ -13,8 +13,7 @@ class PageRB5:
     page = None
     current_button = None
     screen_count = 0
-    screen_list = []
-    test = None
+    screen_index = []
 
     def __init__(self, application, page, index, current_button):
         # Stocke les informations nécessaires au fonctionnement de la page
@@ -25,32 +24,40 @@ class PageRB5:
 
         # Récupère le nombre d'écrans présents
         self.screen_count = QDesktopWidget().screenCount()
-        self.screen_list = [None] * self.screen_count
+        self.screen_index = [None] * self.screen_count
         logging.info("[page_rb5] : " + str(self.screen_count) + " écrans détectés.\n")
 
         # Charge autant de fenêtres que besoins
         for screen_index in range(0, self.screen_count):
             application.engine.load('initialisation/graphics/page_rb/page_rb5/screen_index.qml')
-            print(len(application.engine.rootObjects()))
-            self.screen_list[screen_index] = application.engine.rootObjects()[len(application.engine.rootObjects()) - 1]
-            self.screen_list[screen_index].hide()
+            self.screen_index[screen_index] = application.engine.rootObjects()[len(application.engine.rootObjects()) - 1]
+            self.screen_index[screen_index].hide()
 
         # Change les informations sur chaque pages
+        screen_dimensions = []
         for screen_index in range(0, self.screen_count):
             sg = QDesktopWidget().screenGeometry(screen_index).getCoords()
-            self.screen_list[screen_index].setPosition(sg[0], sg[1])
-            window = self.screen_list[screen_index].findChild(QObject, "screen_index")
+            self.screen_index[screen_index].setPosition(sg[0], sg[1])
+            screen_dimensions.append([sg[2] + 1, sg[3] + 1])
+            window = self.screen_index[screen_index].findChild(QObject, "screen_index")
             window.setProperty("text", str(screen_index + 1))
 
+        # Crée une liste avec la liste des écrans (Aucun + tous les nombres de 1 au nombre d'écrans
+        screen_list = ["Aucun"]
+        for index in range(1, len(screen_dimensions) + 1):
+            screen_list.append(str(index))
 
+        # Envoie liste des fenètre et de leurs dimensions à la graphique de la page*
+        self.page.setProperty("screenList", screen_list)
+        self.page.setProperty("screenSize", screen_dimensions)
 
         # Définit la page comme complète
         application.is_completed[4] = True
 
     def on_page_opened(self, application):
-        for screen_index in self.screen_list:
+        for screen_index in self.screen_index:
             screen_index.show()
 
     def on_page_closed(self, application):
-        for screen_index in self.screen_list:
+        for screen_index in self.screen_index:
             screen_index.hide()
