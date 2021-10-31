f7168337bbcacc46f77ac4455fcfb427fc7533b6
MathieuDenglos <matdenglos@gmail.com>
2021-10-31 21:40:21 +0100

    change log module import in main function

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import traceback
 # Librairies SARDINE
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
-from src.misc.log import log as log
+import src.misc.log.log as log
 import src.initialisation.initialisation_window as ini
 import src.simulation as simulation
 
