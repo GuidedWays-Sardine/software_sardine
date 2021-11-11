a53a36a81ec31b020dd0821bc9bd305f97ac7ef2
MathieuDenglos <matdenglos@gmail.com>
2021-11-11 14:07:19 +0100

    add method to initialise immersion

--- src/simulation.py
+++ src/simulation.py
@@ -40,7 +40,8 @@ class Simulation:
 
         # Indique le début de l'initialisation de la simulation
         initial_time = time.time()
-        log.info("Début de l'initialisation de la simulation.\n\n", prefix="initialisation simulation")
+        log.change_log_prefix("initialisation simulation")
+        log.info("Début de l'initialisation de la simulation.\n\n")
 
         # Initialise l'application
         self.app = app
@@ -83,7 +84,9 @@ class Simulation:
                  str("{:.2f}".format((time.time() - initial_time) * 1000)) + " millisecondes.\n\n",
                  prefix="initialisation simulation")
 
-        # Lance tous les modules (en appelant la fonction run) et vérifie qu'au moins un module a été chargé
+        # Montre les écrans d'immersion (si le mode a été désactivé, aucune fenêtre n'apparaitra
+        self.run_off_screens()
+
         any_launched = False
         for component in self.components:
             if "run" in dir(component):
@@ -99,4 +102,30 @@ class Simulation:
         if not any_launched:
             raise ModuleNotFoundError("Aucun des modules de simulations n'a pu être chargé correctement.\n\n")
 
-        self.app.exec()
+    def initialize_off_screens(self):
+        try:
+            if self.parameters["immersion"]:
+                # Récupère les potentiels indexs des écrans à ne pas éteindre (dû à un logiciel tierce utilisé)
+                # FEATURE : indiquer ici les potentielles exceptions sur les fenêtre nécessitant une autre application
+                exception_list = []
+                # Vérification pour le module de la ligne virtuelle (sur Unreal Engine 5)
+                try:
+                    if self.parameters["sardine simulator.virtual line (ue5).screen_index"] != 0:
+                        exception_list.append(self.parameters["sardine simulator.virtual line (ue5).screen_index"] - 1)
+                except KeyError:
+                    pass
+
+                # Génère toutes les fenêtres noir pour le mode immersion
+                for screen_index in (i for i in range(0, QDesktopWidget().screenCount()) if i not in exception_list):
+                    sg = QDesktopWidget().screenGeometry(screen_index).getCoords()
+                    self.black_screens.append(QMainWindow())
+                    self.black_screens[screen_index].setWindowFlag(Qt.FramelessWindowHint)
+                    self.black_screens[screen_index].setGeometry(sg[0], sg[1], sg[2] - sg[0] + 1, sg[3] - sg[1] + 1)
+                    self.black_screens[screen_index].setStyleSheet("QMainWindow {background: 'black';}")
+                    self.black_screens[screen_index].hide()
+        except KeyError:
+            log.debug("Pas de paramètres \"immersion\". Le mode immersion est désactivé par défaut.\n")
+
+    def run_off_screens(self):
+        for screen in self.black_screens:
+            screen.show()
