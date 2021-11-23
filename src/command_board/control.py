e145caf47880f227080a7916658220161dadffb2
MathieuDenglos <matdenglos@gmail.com>
2021-11-23 23:17:57 +0100

    add enum containing all possibles command board functions and linked them to the appropriate function

--- src/command_board/control.py
+++
@@ -1,2 +0,0 @@
-#ajouter la fonction à appeleler lorsqu'il y a un changement d'état
-#enum pour appeler plus facilement les fonctions
--- src/train/command_board/control.py
+++ src/train/command_board/control.py
@@ -1,3 +1,25 @@
-# TODO Ce fichier contiendra toutes les fonctions à appeler par le pupitre afin de changer de manière sécurisé les données de la base de données train
-# Ce fichier contiendra une class avec une variable pour indiquer si la simulation est encore active
-# Toutes les fonctions
+import os
+import sys
+import traceback
+import threading
+import time
+from enum import Enum, unique
+
+
+# Librairies SARDINE
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src\\")[0]
+sys.path.append(os.path.dirname(PROJECT_DIR))
+import src.misc.log.log as log
+import src.train.command_board.functions as func
+
+
+@unique
+class Actions(Enum):
+    """Enum contenant toutes les actions possibles par le pupitre"""
+    # FEATURE : ajouter les différentes actions en suivant la structure NOM_ACTION = (func.fonction,) relié à functions.py
+    LEVER_PANTHO = (func.lever_panto,)
+    MANIP_DE_TRACTION = (func.manip_de_traction,)
+
+    # Fonction permettant de facilement appeler la fonction associée à la valeur de l'Enum
+    def __call__(self, *args, **kwargs):
+        self.value[0](*args, **kwargs)
