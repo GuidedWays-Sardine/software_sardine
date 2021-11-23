48ec419e263879ea280ea7f5fc781f873b912938
MathieuDenglos <matdenglos@gmail.com>
2021-11-23 23:27:51 +0100

    add comment to indicate how functions shall be build

--- src/train/command_board/functions.py
+++ src/train/command_board/functions.py
@@ -10,6 +10,11 @@ sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
 
 
+# FEATURE : ajouter les différentes fonctions ici. Chaque fonction doit recevoir :
+# - La base de données train pour modifier l'état des systèmes
+# - Le temps où le changement d'état a été appelé (par défaut time.time() si pas fournis)
+# - Potentiellement une valeur (par exemple pour les sorties analogiques (toujours trouver une valeur par défaut)
+
 def lever_panto(self, database, time=time.time()):
     pass
 
