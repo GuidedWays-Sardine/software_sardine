ef6a81364b73b96d967b5b7990b17cd787adbde2
MathieuDenglos <matdenglos@gmail.com>
2021-11-24 09:23:48 +0100

    modification

--- src/line/line_generation/line_generation.py
+++ src/line/line_generation/line_generation.py
@@ -7,181 +7,47 @@ import time
 # Librairies de traitement de données
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 #librairies SARDINE
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
-
-
-# Chemin d'accès vers les données
-DATA_DIR = PROJECT_DIR + "src\\line\\raw_data\\"
-
-
-# Constantes sur les noms des colonnes
-GEO_LO = "GEO_LO"
-GEO_LA = "GEO_LA"
-PK = "PK"
-DEBUT = "_D"
-FIN = "_F"
+import src.line.line_generation.database as DB
 
 
 class LineGenerator:
-    # Lignes existantes sur le RFN
-    lines_link = "https://ressources.data.sncf.com/explore/dataset/lignes-par-statut/table/"
-    lines_path = DATA_DIR + "line\\lignes-par-statut.csv"
-    lines_attr = ["CODE_LIGNE", "LIB_LIGNE", "STATUT", "PKD", "PKF", "C_GEO_D", "C_GEO_F"]
-    lines = None
+    """Classe contenant toute la logique pour générer la ligne à partir des bases de données"""
 
-    # Liste des voies sur le RFN
-    tracks_link = "https://ressources.data.sncf.com/explore/dataset/fichier-de-formes-des-voies-du-reseau-ferre-national/table/"
-    tracks_path = DATA_DIR + "line\\fichier-de-formes-des-voies-du-reseau-ferre-national.csv"
-    tracks_attr = ["CODE_LIGNE", "NOM_VOIE", "PK_DEBUT_R", "PK_FIN_R", "Geo Point"]
-    tracks = None
+    # Bases de données nécessaires à la génération de la ligne
+    lines = DB.DataBase("line\\lignes-par-statut.csv",
+                        ["CODE_LIGNE", "LIB_LIGNE", "STATUT", "PKD", "PKF", "C_GEO_D", "C_GEO_F"],
+                        "https://ressources.data.sncf.com/explore/dataset/lignes-par-statut/table/")
 
+    tracks = DB.DataBase("line\\fichier-de-formes-des-voies-du-reseau-ferre-national.csv",
+                         ["CODE_LIGNE", "NOM_VOIE", "PK_DEBUT_R", "PK_FIN_R", "Geo Point"],
+                         "https://ressources.data.sncf.com/explore/dataset/fichier-de-formes-des-voies-du-reseau-ferre-national/table/")
 
-    # Données reliées aux courbes
-    curves_link = "https://ressources.data.sncf.com/explore/dataset/courbe-des-voies/table/"
-    curves_path = DATA_DIR + "line\\courbe-des-voies.csv"
-    curves_attr = ["CODE_LIGNE", "SENS", "RAYON", "PKD", "PKF", "C_GEO_D", "C_GEO_F"]
-    curves = None
+    curves = DB.DataBase("line\\courbe-des-voies.csv",
+                         ["CODE_LIGNE","NOM_VOIE", "SENS", "RAYON", "PKD", "PKF", "C_GEO_D", "C_GEO_F"],
+                         "https://ressources.data.sncf.com/explore/dataset/courbe-des-voies/table/")
 
-    # Données reliées aux déclivités
-    slopes_link = "https://ressources.data.sncf.com/explore/dataset/caracteristique-des-voies-et-declivite/table/"
-    slopes_path = DATA_DIR + "line\\caracteristique-des-voies-et-declivite.csv"
-    slopes_attr = ["CODE_LIGNE", "NOM_VOIE", "TYPE", "VALEUR", "PKD", "PKF", "C_GEO_D", "C_GEO_F"]
-    slopes = None
+    slopes = DB.DataBase("line\\caracteristique-des-voies-et-declivite.csv",
+                         ["CODE_LIGNE", "NOM_VOIE", "TYPE", "VALEUR", "PKD", "PKF", "C_GEO_D", "C_GEO_F"],
+                         "https://ressources.data.sncf.com/explore/dataset/caracteristique-des-voies-et-declivite/table/")
 
-    # Données reliées à la vitesse maximale
-    max_speed_link = "https://ressources.data.sncf.com/explore/dataset/vitesse-maximale-nominale-sur-ligne/table/"
-    max_speed_path = DATA_DIR + "line\\vitesse-maximale-nominale-sur-ligne.csv"
-    max_speed_attr = ["CODE_LIGNE", "V_MAX", "PKD", "PKF", "C_GEO_D", "C_GEO_F"]
-    max_speed = None
+    max_speed = DB.DataBase("line\\vitesse-maximale-nominale-sur-ligne.csv",
+                            ["CODE_LIGNE", "V_MAX", "PKD", "PKF", "C_GEO_D", "C_GEO_F"],
+                            "https://ressources.data.sncf.com/explore/dataset/vitesse-maximale-nominale-sur-ligne/table/")
 
-    # Données reliées à l'électrification des lignes
-    electrification_link = "https://ressources.data.sncf.com/explore/dataset/liste-des-lignes-electrifiees/table/"
-    electrification_path = DATA_DIR + "line\\liste-des-lignes-electrifiees.csv"
-    electrification_attr = ["CODE_LIGNE", "ELECT", "PKD", "PKF", "C_GEO_D", "C_GEO_F"]
-    electrification = None
+    electrification = DB.DataBase("line\\liste-des-lignes-electrifiees.csv",
+                                  ["CODE_LIGNE", "ELECT", "PKD", "PKF", "C_GEO_D", "C_GEO_F"],
+                                  "https://ressources.data.sncf.com/explore/dataset/liste-des-lignes-electrifiees/table/")
 
     def __init__(self):
-        """Initialise le générateur de ligne en chargeant toutes les bases de données"""
-        initial_time = time.time()
-        log.change_log_prefix("initialisation des données lignes")
-        log.info("Chargement des bases de données pour la génération des lignes.\n")
-
-        # Chargement des lignes existantes et de la liste des voies sur le RFN
-        self.lines = pd.read_csv(self.lines_path, delimiter=";", usecols=self.lines_attr)[self.lines_attr]
-        self.tracks = pd.read_csv(self.tracks_path, delimiter=";", usecols=self.tracks_attr)[self.tracks_attr]
-        # Chargement des éléments nécessaires à la génération de la ligne (courbe des voies, déclivités, vitesses maximales)
-        #self.curves = pd.read_csv(self.curves_path, delimiter=";", usecols=self.curves_attr)[self.curves_attr]
-        #self.slopes = pd.read_csv(self.slopes_path, delimiter=";", usecols=self.slopes_attr)[self.slopes_attr]
-        #self.max_speed = pd.read_csv(self.max_speed_path, delimiter=";", usecols=self.max_speed_attr)[self.max_speed_attr]
-        #self.electrification = pd.read_csv(self.electrification_path, delimiter=";", usecols=self.electrification_attr)[self.electrification_attr]
-
-        # Pour chacunes des bases de données chargées
-        for df in (df for df in [self.lines, self.tracks, self.curves, self.slopes, self.max_speed, self.electrification
-                                 ] if df is not None):
-            # Récupère la liste des colonnes
-            df_columns = list(df.columns)
-
-            # Pour toutes les colonnes mentionnant des positions géographiques
-            # (Actuellement les colonnes C_GEO sont de la forme : "GEO_longitude,GEO_latitude")
-            geo_index = [g_i for g_i in range(len(df_columns)) if "geo" in df_columns[g_i].lower()]
-            for i in range(0, len(geo_index)):
-                # Récupère la liste, sépare et convertit en float, les positions de longitude et de latitude
-                split_geo_points = df[df_columns[geo_index[i] + i]].str.split(",", expand=True).astype(np.float64)
-
-                # Change les valeurs de la colonne existante pour la longitude et en insert une nouvelle pour la latitude
-                df[df_columns[geo_index[i] + i]] = split_geo_points[0]
-                df.insert(geo_index[i] + i + 1, GEO_LA + str(i), split_geo_points[1])
-
-                # Change le nom des colonnes pour garder un format compréhensible et normalisé
-                df_columns[geo_index[i] + i] = GEO_LO + (DEBUT if "D" in df_columns[geo_index[i] + i]
-                                                         else FIN if "F" in df_columns[geo_index[i] + i]
-                                                         else "")
-                df_columns.insert(geo_index[i] + i + 1, GEO_LA + (DEBUT if "D" in df_columns[geo_index[i] + i]
-                                                                  else FIN if "F" in df_columns[geo_index[i] + i]
-                                                                  else ""))
-                # Le nom des colonnes dans la dataFrame seront changés en fin de processus
-
-            # Pour toutes les colonnes mentionnant des Points Kilométriques
-
-
-            # Change le nom des colonnes
-            df.columns = df_columns
-            print(df.dtypes)
-
-
-        for col in ["PKD", "PKF"]:
-            self.lines[col] = np.where(self.lines[col].str.contains("-"), "-", "") + self.lines[col].str.replace("[-+]", ".", regex=True)
-            self.lines.loc[self.lines[col].str.contains("^[a-zA-Z].*", regex=True), col] = (self.lines[col].str.get(0).apply(ord) - 64).astype("string") + self.lines[col].str[1:]
-            self.lines[col] = self.lines[col].astype(np.float32)
-        #print(self.lines.dtypes)
-        #print(self.lines.loc[330:350, ["CODE_LIGNE", "PKD", "PKF"]])
-
-
-
-        # Indique le temps de chargement des bases de données
-        log.info("Chargement de toutes les bases de données en " +
-                 str("{:.2f}".format((time.time() - initial_time)*1000)) + " milisecondes.\n\n")
-
-    def test(self, line_code):
-        """
-        .head() -> affiche les premiers
-        .tail() -> affiche les derniers
-        .shape -> nb lignes, nb_atributes
-        .describe() -> donne le nombre de valeurs, le nombre de valeurs différentes et la valeur avec la valeur la plus élevée
-        .columns -> Le nom des colonnes
-        .index -> le numéro des lignes (avec pandas, l'index est indépendant de [])
-        .columns = ["", "",...]-> pour renommer les colonnes
-        ["column_name] -> retourne la colonne avec se nom en format Series (DataFrame avec un seul colonne)
-        .loc[index, "column_name"]      attention index
-        .loc[index, "first_column":"end_column"]
-        ["column"][index]
-        .loc[index]["column_index"]
-        .loc[5, ["column_1", "column_2", ...]]
-        .iloc[index, index_column]
-        .iloc[index, index_columb_begin:index_column_end]
-        .iloc/loc[index_begin:index_end, ...]
-        ["column_name"].mean()
-
-        .append({"column_1":...})
-
-
-
-        mask = ...["column_1", ...] > value
-        si plusieurs colomnes, valeurs comparés sur plusieurs colones
-        stock colomne avec le résultat pour toutes les lignes. Permet de rendre le tableau plus lisible
-
-        ...[mask]
-
-        .dtypes -> type de chaque colonne
-        si string ou truc au pif -> object
-        df.index[0]     -> index de la première ligne
-        .drop[index, inplace=True]      -> inplace indique si le nouveau tableau doit être retourné, ou s'il doit être stocké dans le tableau actuel
-        .to_list() -> transforme la Series en Liste
-        .dropna() -> supprime toutes les colonnes qui contient un NaN dans une des valeurs
-        .drop_duplicate()   -> supprime toutes les lignes en doubles
-        .reset_index(drop=True)      -> reset tous les index (0, 1,...) (drop sinon ça garde les anciens index dans une colonnes)
-        .where(mask, inplace=True)  -> remplace toutes les cas ou le mask retourne true en NaN
-        .replace(old_value, new_value)  -> à regarder plus en détail ?
-        .isin([])       -> regarde si la valeur est cases sont dans cette list
-        .copy()         -> renvoie une copie (pas un pointeur)
-        .datetime()     -> pour les dates ?
-        np.vectorize(lambda...)     -> parfois rapide que juste df["column_name"] = ...
-        ~signifie l'inverse
-        .concat()       -> existe mais peu utile pour le projet
-        .merge()        -> existe mais peu utile pour le projet
-        regex (re)      -> pour des recherches pas complètes
-        df["A"] = pd.to_numeric(df["A"], downcast="float")
-        df["column_name"].astype(type)      -> attention string object donc possibilité que ça ne marche pas
-        pas de for dans les tableaux        -> truc que j'ai loupé -> vectorisation pour l'optimiser
-        https://www.google.com/url?sa=i&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F52783882%2Fpandas-and-numpy-program-slower-than-loop-version-of-same-functionality-how-to&psig=AOvVaw1X5Q98avR9f1Ipj9jDxT2k&ust=1637102449664000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDlvYO4m_QCFQAAAAAdAAAAABAD
-        .sort pour trier()
-        """
-        print(self.lines.head(10))
+        """Initialise la liste des lignes"""
+        self.lines.load()
 
     def generate_line(self, line_code, reload=False):
         """Fonction permettant de générer les données lignes (Python et UE5) à partir du code ligne
@@ -191,26 +57,89 @@ class LineGenerator:
         line_code: `int`
             Code de la ligne (trouvable en ligne selon le nom de la ligne
         """
-        # Vérifie que la ligne n'existe pas encore et si non la recharge
-        if str(line_code) in os.listdir(PROJECT_DIR + "settings\\line_settings\\" + str(line_code)) and not reload:
+        # Si la ligne existe déjà (enregistrée dans settings/line_settings/line_code) et qu'elle ne doit pas être rechargée, retourne
+        if str(line_code) in os.listdir(PROJECT_DIR + "settings\\line_settings") and not reload:
             log.info("La ligne " + str(line_code) + " existe déjà. Elle ne sera pas recréée.\n\n")
             return
 
         # Vérifie que la ligne qu'on essaye de charger existe
-        line_name = self.lines.loc[self.lines["CODE_LIGNE"] == line_code]
+        line_name = list(self.lines.df.loc[self.lines.df["CODE_LIGNE"] == line_code, "LIB_LIGNE"])
         if not line_name:
             log.warning("Aucune ligne existance avec le code_ligne : " + str(line_code) + ".\n\n")
             return
 
+        # Sinon commence par charger les bases de données ligne (si ce n'est pas déjà fait)
+        self.load_line_databases()
+
         # Sinon indique le nom et les informations clés de la ligne en question
+        log.info("Génération de la ligne " + line_name[0] + ".\n")
+
+        df_line = self.curves.df.loc[self.curves.df["CODE_LIGNE"] == line_code].sort_values(DB.PK + DB.DEBUT).reset_index()
+        #print(list(self.curves.df["NOM_VOIE"].unique()))
+        #print(self.curves.df.loc[self.curves.df["NOM_VOIE"].str.contains("4CIRC", regex=False)][["CODE_LIGNE", "NOM_VOIE", 'PK_D', 'GEO_LO_D', "GEO_LA_D"]])
+        print(df_line["NOM_VOIE"].unique())
+        print(df_line.loc[df_line["NOM_VOIE"].str.contains("J5966", regex=False)].sort_values("PK_D")[['PK_D', "PK_F", "RAYON", 'GEO_LO_D', "GEO_LA_D"]])
+
+    def load_line_databases(self):
+        """Charge toutes les bases de données reliées à la ligne"""
+        initial_time = time.time()
+        log.change_log_prefix("chargement des bases de données lignes")
+        log.info("Chargement des bases de données pour la génération des lignes")
+
+        # Chargement base de données voies
+        self.tracks.load()
+
+        # Chargement bases de données courbes
+        if self.curves.load():
+            # Gestion des cas où le rayon contient une valeur non lisible (le convertit en alignement et passe le rayon à 0)
+            self.curves.df.loc[self.curves.df["RAYON"].str.contains("^[a-zA-Z].*", regex=True), "SENS"] = "ALIGNEMENT"
+            self.curves.df.loc[self.curves.df["RAYON"].str.contains("^[a-zA-Z].*", regex=True), "RAYON"] = "0"
+            self.curves.df["RAYON"] = self.curves.df["RAYON"].astype(np.int16)
+
+        # Chargement des pentes et rampes
+        if self.slopes.load():
+            # Gestion du problème lié au pentes et rampes exprimés en % avec des ","
+            self.slopes.df["VALEUR"] = np.where(self.slopes.df["VALEUR"].str.contains(","),
+                                                self.slopes.df["VALEUR"].str.replace(",", ""),
+                                                self.slopes.df["VALEUR"] + "0").astype(np.int8)
+
+        # Chargement de la vitesse max pas tronçons
+        self.max_speed.load()
+
+        # Chargement de l'électrification des lignes
+        self.electrification.load()
+
+        # Indique le temps de chargement des bases de données
+        log.info("Chargement de toutes les bases de données génération de ligne en " +
+                 str("{:.2f}".format((time.time() - initial_time)*1000)) + " milisecondes.\n\n")
 
 
+def get_distance_to_line(line_begin, line_end, point):
+    """
 
+    :param line_:
+    :param la_d:
+    :param lo_f:
+    :param la_f:
+    :param x:
+    :param y:
+    :return:
+    """
+    # ax1 + b = y1      =>      a = (x1-x2)/(y1+y2)
+    # ax2 + b = y2      =>      b = y1-x1*(x1-x2)/(y1+y2)
+    a = (line_begin[0] - line_end[0])/(line_begin[1] + line_end[1])
+    b = line_begin[1] - line_begin[0] * a
+
+    # d =  |y - ax - b|/sqrt(1 + a²)
+    print(abs(point[1] - a * point[1] - b) / ((1 + a ** 2) ** 0.5))
 
 if __name__ == "__main__":
-    log.initialise("../../../log", "1.1.0", log.Level.DEBUG)
+    #log.initialise("../../../log", "1.1.0", log.Level.DEBUG)
 
     # Codes pour la LGV Sud-Est
-    line_generator = LineGenerator()
+    #line_generator = LineGenerator()
     #line_generator.generate_line(752000)
+
+    get_distance_to_line([0, 0], [2, 2], [1, 1])
+
 
