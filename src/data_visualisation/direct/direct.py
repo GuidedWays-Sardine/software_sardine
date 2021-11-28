4145f5caac277700fb4dbf687b07bd38f9cf60c9
remye <remy.evrard.1@gmail.com>
2021-11-28 21:23:53 +0100

    Integration of a graph family selection interface and support for displaying multiple graphs automatically arranged on a grid.

--- src/data_visualisation/direct/direct.py
+++ src/data_visualisation/direct/direct.py
@@ -9,16 +9,16 @@ import os
 
 # Librairies graphiques
 import numpy as np
-from PyQt5.QtWidgets import QApplication
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+
 from matplotlib.backends.qt_compat \
     import QtCore, QtWidgets
 
-if QtCore.qVersion() >= "5.":
-    from matplotlib.backends.backend_qt5agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
-else:
-    from matplotlib.backends.backend_qt4agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+
 from matplotlib.figure import Figure
 
 # Librairies SARDINE
@@ -30,11 +30,14 @@ VERSION = "1.0.0"
 INITIAL_LOGGING = log.Level.DEBUG
 
 
-class DashboardWindow(QtWidgets.QMainWindow):
+#class DashboardWindow(QtWidgets.QMainWindow):
+class DashboardWindow(QMainWindow):
     layout = ""
 
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Dashboard graphiques en direct")
 
         """ 
         Initialisation du log
@@ -47,20 +50,82 @@ class DashboardWindow(QtWidgets.QMainWindow):
         Création de la fenêtre
         """
         # Créé le widget parent (la fenêtre)
-        self.window = QtWidgets.QWidget()
+        self.window = QWidget()
 
+        # Layout contenant la fenêtre principale
+        self.main_layout = QHBoxLayout()
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+
+        # Layout contenant les graphiques
+        self.graph_main_layout = QStackedLayout()
+
+
+        # Initialise le sélecteur de graphe
+        self.graph_type_selector = QListWidget()
+        graph_type_list = ["ERTMS", "Vitesse et dérivées", "Profil ligne"]
+        for i in range(len(graph_type_list)):
+            item = QListWidgetItem(graph_type_list[i])
+            item.setTextAlignment(Qt.AlignCenter)
+            self.graph_type_selector.addItem(item)
+
+        # Écoute le signale du sélecteur pour changer de graphique
+        self.graph_type_selector.itemSelectionChanged.connect(self.change_graph_type_display)
+
+
+        # Ajoute le sélecteur de graphique sur la fenêtre principale
+        self.main_layout.addWidget(self.graph_type_selector, 1)
+
+
+        # main_layout => graph_main_layout => Widget(graph_layout)
+
+
+        # Rempli le tableau des layout des types de graphiques.
+        # Chaque type de graphique (ERTMS, vitesse, ...) a son propre layout
+        # et nous les stockons dans un tableau de layout qui lui même sera ajouté
+        # à un layout principal des graphiques
+        self.graph_layout = []
+        # Position en x (ligne) du graphique dans la grille
+        self.grid_i_layout = []
+        # Position en y (colonne) du graphique dans la grille
+        self.grid_j_layout = []
+        # Widget container permettant de stocker le layout de type grille
+        # (le layout parent QStackedLayout n'accepte que des widgets)
+        self.graph_container_of_layout = []
+
+
+        for i in range(len(graph_type_list)):
+            self.graph_container_of_layout.append(QtWidgets.QWidget())
+
+            # Place le container dans le layout grille
+            grid_layout_to_add = QGridLayout(self.graph_container_of_layout[i])
+
+            # Ajoute la layout grille à la
+            self.graph_layout.append(grid_layout_to_add)
+            # Initialise la position (i, j) du premier graphique dans le layout
+            self.grid_i_layout.append(0)
+            self.grid_j_layout.append(0)
+
+
+        # Ajoute les layout des types de graphique au layout principal des graphiques
+        for i in range(len(self.graph_layout)):
+            self.graph_main_layout.addWidget(self.graph_container_of_layout[i])
+
+
+        # Ajoute les graphiques sur la fenêtre principale
+        self.main_layout.addLayout(self.graph_main_layout, 10)
+
+        # Défini le layout principal
+        self.window.setLayout(self.main_layout)
         # Indique que le widget principal (la fenêtre) est self.window
         self.setCentralWidget(self.window)
 
-        # Créé une grille (layout) et la place dans le widget principal (la fenêtre self.window)
-        self.layout = QtWidgets.QVBoxLayout(self.window)
 
         """ 
-        Initialisation du graphique
+        Initialisation des graphiques
         """
-        # Premier graphique
+        # GRAPHIQUE 1 ERTMS
         # Initialisation des plots du graphique
-        test_graph_data = np.array([[1, 190, 175, 160, 160],
+        ertms_graph_1_data = np.array([[1, 190, 175, 160, 160],
                                      [2, 190, 175, 160, 160],
                                      [3, 190, 175, 160, 140],
                                      [4, 190, 175, 140, 120],
@@ -72,24 +137,117 @@ class DashboardWindow(QtWidgets.QMainWindow):
                                      [10, 190, 80, 65, 75]])
 
         # Indique si les courbes doivent être remplies
-        test_graph_is_filled = [True, True, True, True]
+        ertms_graph_1_is_filled = [True, True, True, True]
 
         # Couleur de remplissage des courbes
-        test_graph_fill_color = ["#C00000", "#EA9100", "#DFDF00", "#969696"]
+        ertms_graph_1_fill_color = ["#C00000", "#EA9100", "#DFDF00", "#969696"]
 
         # Initialisation du graphique
-        test_graph = Graph(self, "", True, "Graphique demonstratif", "Distance", "Vitesse", test_graph_data,
-                            False, 250, True, test_graph_is_filled, test_graph_fill_color)
+        ertms_graph_1 = Graph(self, "ertms", True, "ERTMS - Relevé 1", "Distance (km)", "Vitesse (km/h)", ertms_graph_1_data,
+                            False, 250, True, ertms_graph_1_is_filled, ertms_graph_1_fill_color)
 
         # Test d'ajout de données
         # Déclaration des valeurs à ajouter
-        test_graph_plot_to_add = np.array([[11, 190, 175, 160, 160],
-                                            [12, 190, 175, 160, 160],
-                                            [13, 190, 175, 160, 140],
-                                            [14, 190, 175, 140, 120]])
+        ertms_graph_1_plot_to_add = np.array([[11, 190, 175, 160, 160],
+                                             [12, 190, 175, 160, 160],
+                                             [13, 190, 175, 160, 140],
+                                             [14, 190, 175, 140, 120]])
 
         # Ajout des valeurs au graphique
-        test_graph.add_values(test_graph_plot_to_add, False, True)
+        ertms_graph_1.add_values(ertms_graph_1_plot_to_add, False, True)
+
+
+        # GRAPHIQUE 2 ERTMS
+        # Initialisation des plots du graphique
+        ertms_graph_2_data = np.array([[1, 190, 175, 160, 160],
+                                       [2, 190, 175, 160, 160],
+                                       [3, 190, 175, 160, 140],
+                                       [4, 190, 175, 140, 120],
+                                       [5, 190, 155, 120, 120]])
+
+        # Indique si les courbes doivent être remplies
+        ertms_graph_2_is_filled = [True, True, True, True]
+
+        # Couleur de remplissage des courbes
+        ertms_graph_2_fill_color = ["#C00000", "#EA9100", "#DFDF00", "#969696"]
+
+        # Initialisation du graphique
+        ertms_graph_2 = Graph(self, "ertms", True, "ERTMS - Relevé 2", "Distance (km)", "Vitesse (km/h)",
+                              ertms_graph_2_data,
+                              False, 250, True, ertms_graph_2_is_filled, ertms_graph_2_fill_color)
+
+
+        # GRAPHIQUE 1 VITESSE ET ACCELERATION
+        vit_et_acc_graph_1_data = np.array([[1, 190],
+                                            [2, 191],
+                                            [3, 192],
+                                            [4, 193],
+                                            [5, 193],
+                                            [6, 192.5],
+                                            [7, 193],
+                                            [8, 194],
+                                            [9, 195],
+                                            [10, 195.2]])
+
+        # Indique si les courbes doivent être remplies
+        vit_et_acc_graph_1_is_filled = [False]
+
+        # Couleur de remplissage des courbes
+        vit_et_acc_graph_1_fill_color = [""]
+
+
+        vit_et_acc_graph_1 = Graph(self, "vitesse_et_derive", True, "Vitesse", "Temps (s)", "Vitesse (km/h)", vit_et_acc_graph_1_data,
+                            False, 250, True, vit_et_acc_graph_1_is_filled, vit_et_acc_graph_1_fill_color)
+
+        # GRAPHIQUE 2 VITESSE ET ACCELERATION : accélération
+        vit_et_acc_graph_2_data = np.array([[1, 0],
+                                            [2, 0.01],
+                                            [3, 0.01],
+                                            [4, 0.01],
+                                            [5, 0.01],
+                                            [6, 0.005],
+                                            [7, 0.01],
+                                            [8, 0.01],
+                                            [9, 0.01],
+                                            [10, 0.003]])
+
+        # Indique si les courbes doivent être remplies
+        vit_et_acc_graph_2_is_filled = [False]
+
+        # Couleur de remplissage des courbes
+        vit_et_acc_graph_2_fill_color = [""]
+
+        vit_et_acc_graph_2 = Graph(self, "vitesse_et_derive", True, "Accélération longitudinale", "Temps (s)", "Accélération (m/s²)",
+                                   vit_et_acc_graph_2_data,
+                                   False, 250, True, vit_et_acc_graph_2_is_filled, vit_et_acc_graph_2_fill_color)
+
+        # GRAPHIQUE 3 VITESSE ET ACCELERATION : jerk
+        vit_et_acc_graph_3_data = np.array([[1, 0],
+                                            [2, 0.001],
+                                            [3, 0.001],
+                                            [4, 0.001],
+                                            [5, 0.001],
+                                            [6, 0.0005],
+                                            [7, 0.001],
+                                            [8, 0.001],
+                                            [9, 0.001],
+                                            [10, 0.0003]])
+
+        # Indique si les courbes doivent être remplies
+        vit_et_acc_graph_3_is_filled = [False]
+
+        # Couleur de remplissage des courbes
+        vit_et_acc_graph_3_fill_color = [""]
+
+        vit_et_acc_graph_2 = Graph(self, "vitesse_et_derive", True, "Jerk norme euclidienne", "Temps (s)", "Jerk (m/s³)",
+                                   vit_et_acc_graph_3_data,
+                                   False, 250, True, vit_et_acc_graph_3_is_filled, vit_et_acc_graph_3_fill_color)
+
+
+
+
+
+
 
         """
         Log d'application
@@ -99,13 +257,35 @@ class DashboardWindow(QtWidgets.QMainWindow):
                  str("{:.2f}".format((time.time() - initial_time) * 1000)) + " millisecondes.\n\n")
 
 
-    def add_widget(self, widget_to_add):
+    def add_widget(self, widget_to_add, graph_type):
         """ Ajoute un widget dans le layout
         Parameters
         ----------
         widget_to_add -- widget Qt à ajouter au layout
         """
-        self.layout.addWidget(widget_to_add)
+        # Récupère le type de graphique permettant de placer le graphique dans le bon layout de type grille
+        if graph_type == "ertms":
+            id_graph_layout = 0
+
+        if graph_type == "vitesse_et_derive":
+            id_graph_layout = 1
+
+        # Récupère le nnombre actuel de graphique dans la layout de graphique
+        # permettant de savoir si un passage à la ligne pour l'affichage du
+        # nouveau graphique est nécessaire ou non
+        number_of_graph_in_layout = self.graph_layout[id_graph_layout].count()
+
+        # Si le nombre de graphique est pair, alors on place le nouveau graphique à la ligne suivante
+        if number_of_graph_in_layout % 2 == 0 and number_of_graph_in_layout != 0:
+            self.grid_i_layout[id_graph_layout] += 1
+            self.grid_j_layout[id_graph_layout] = 0
+
+        # S'il n'y a qu'un seul graphique sur la ligne, on met notre graphique à la suite
+        if number_of_graph_in_layout % 2 == 1:
+            self.grid_j_layout[id_graph_layout] += 1
+
+        # Ajoute le graphique à la case (i, j) définie du layout
+        self.graph_layout[id_graph_layout].addWidget(widget_to_add, self.grid_i_layout[id_graph_layout], self.grid_j_layout[id_graph_layout])
 
 
     def add_navigation_toolbar(self, concerned_widget):
@@ -115,6 +295,14 @@ class DashboardWindow(QtWidgets.QMainWindow):
         concerned_widget -- graphique auquel rattacher la barre d'outil
         """
         self.addToolBar(NavigationToolbar(concerned_widget, self))
+
+
+    def change_graph_type_display(self):
+        """ Modifie le layout de premier plan changeant ainsi le type de graphique
+        """
+        # print("Current list item :", self.graph_type_selector.currentRow())
+        self.graph_main_layout.setCurrentIndex(self.graph_type_selector.currentRow())
+
 
 
 class Graph(DashboardWindow):
@@ -175,7 +363,7 @@ class Graph(DashboardWindow):
         self.graph_title = graph_title
         self.x_label = x_label
         self.y_label = y_label
-        self.graph_type = graph_type        # TODO : à implémenter
+        self.graph_type = graph_type
         self.is_dynamic = is_dynamic        # TODO : à implémenter
         self.refresh_rate = refresh_rate    # TODO : à implémenter
         self.has_toolbar = has_toolbar
@@ -188,7 +376,7 @@ class Graph(DashboardWindow):
 
         # Initialise et affiche le graphique si seulement celui-ci est visible
         if is_visible:
-            self.window.add_widget(self.graph)
+            self.window.add_widget(self.graph, self.graph_type)
 
             # Créé la figure et l'ensemble des tracés graphiques
             self.data_graph_ax = self.graph.figure.subplots()  # Récupère le système d'axe existant sur self.graph
@@ -309,8 +497,18 @@ class Graph(DashboardWindow):
 
         # Replotte individuellement chaque courbe et la re-remplie si demandée
         for i in range(len(y_data)):
-            self.data_graph_line, = self.data_graph_ax.plot(x_data[0], y_data[i], alpha=0)
+            # Si le graphique est rempli, on ne trace pas les lignes de limitation de la courbe
+            if self.is_filled[i - 1] == True:
+                alpha_lines = 0
 
+            # Si le grapique n'est pas rempli, on trace les lignes de limitation de la courbe
+            if self.is_filled[i - 1] == False:
+                alpha_lines = 1
+
+            # Tracé du graphique
+            self.data_graph_line, = self.data_graph_ax.plot(x_data[0], y_data[i], alpha=alpha_lines)
+
+            # Remplissage du graphique
             if self.is_filled[i - 1] == True:
                 self.data_graph_ax.fill_between(x_data[0], y_data[i], color=self.fill_color[i])
 
@@ -326,10 +524,15 @@ def main():
     # Vérifie s'il n'y a pas une instance de QApplication en cours d'execution
     qapp = QApplication.instance()
     if not qapp:
+        # On passe sys.argv pour activer le contrôle par ligne de commande
         qapp = QApplication(sys.argv)
 
     # Défini le style de l'application
     qapp.setStyle('Fusion')
+
+    with open("dashboard.qss", "r") as f:
+        _style = f.read()
+        qapp.setStyleSheet(_style)
 
     # TODO : supporter le chargement de fichier QML
     # https://doc.qt.io/qtforpython/tutorials/qmlintegration/qmlintegration.html
