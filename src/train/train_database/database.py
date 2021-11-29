9fb99509fbe4b31ea6f85ae4dab7b57c10ca3be5
malo <malo.guilhelacombedevillers@estaca.eu>
2021-11-29 18:05:34 +0100

    create train database branch

--- src/train/train_database/database.py
+++ src/train/train_database/database.py
@@ -1,7 +1,6 @@
 # Librairies par défaut python
 import sys
 import os
-import threading
 
 
 # Librairies SARDINE
@@ -12,14 +11,11 @@ import src.misc.log.log as log
 
 class TrainDatabase:
     # Données du train
-    lock = threading.Lock()
-    speed = 0
+
 
 
     # TODO : travailler la structure et rajouter tous les
 
 
     def __init__(self, train_data):
-        #TODO : faire une fonction qui récupère les données du trains et qui complète la base de donnée
-        with self.lock:
-            log.info("Début de l'initialisation de la base de donnée train.", prefix="Train database")
+        pass
