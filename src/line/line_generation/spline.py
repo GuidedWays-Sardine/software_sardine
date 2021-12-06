a3f99d2592709854e0e474cd538bc25403dfc86c
MathieuDenglos <matdenglos@gmail.com>
2021-12-06 23:44:46 +0100

    add structures to functions to initialise, update and split a spline

--- src/line/line_generation/spline.py
+++ src/line/line_generation/spline.py
@@ -60,10 +60,30 @@ class Spline:
         self.geo_fin = [track[DB.GEO_LO + DB.FIN], track[DB.GEO_LA + DB.FIN]]
 
         # Stocke les bases de données avec les informations sur la voie
-        self.curves = curves
-        self.slopes = slopes
-        self.max_speed = max_speed
-        self.electrification = electrification
+        self.curves = curves.reset_index(drop=True)
+        self.slopes = slopes.reset_index(drop=True)
+        self.max_speed = max_speed.reset_index(drop=True)
+        self.electrification = electrification.reset_index(drop=True)
 
-    def __init__(self):
-        print("e")
+    def update(self, curves, slopes, max_speed, electrification):
+        pass
+        # TODO : fonction permettant de mettre à jour la base de données
+
+    def clean_databases(self):
+        """Fonction permettant de s'assurer que toutes les valeurs sont dans les limites de la voie"""
+        # Pour chacunes des bases de données contenus dans la classe
+        for df in [self.curves, self.slopes, self.max_speed, self.electrification]:
+            # Remplace tous les PK et positions géographiques de début et fin pour les points qui sortent de la limite
+            df.loc[(df[DB.PK + DB.DEBUT] < self.pk_debut), [DB.GEO_LO + DB.DEBUT, DB.GEO_LA + DB.DEBUT, DB.PK + DB.DEBUT]] = \
+                [self.geo_debut[0], self.geo_debut[1], self.pk_debut]
+            df.loc[(df[DB.PK + DB.FIN] < self.pk_fin), [DB.GEO_LO + DB.FIN, DB.GEO_LA + DB.FIN, DB.PK + DB.FIN]] = \
+                [self.geo_fin[0], self.geo_fin[1], self.pk_fin]
+
+    def is_completed(self):
+        """Fonction indiquant si les informations pour la spline sont complétées ou non"""
+        return not self.curves.empty and not self.slopes.empty and self.electrification.empty and self.max_speed.empty
+        #TODO : améliorer la vérification
+
+    def split(self):
+        # Fonction qui à partir d'un PK split la spline en deux
+        pass
