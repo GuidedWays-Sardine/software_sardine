3f3c0cf9ab215fed1eeb21a02824fc7e53004fcf
MathieuDenglos <matdenglos@gmail.com>
2021-12-06 23:47:13 +0100

    add structures to mathematical functions of spline (to get distance from line or get position of kilometric point

--- src/line/line_generation/spline.py
+++ src/line/line_generation/spline.py
@@ -102,3 +102,119 @@ class Spline:
         """Fonction permettant de convertir et d'enregistrer les informations d'une spline"""
         # Fonction permettant de convertir en une spline compatible pour UE 5
         pass
+
+    # Fonctions mathématiques utiles au foncctionnement des splines
+    def get_distance_to_spline(self, pk, point):
+        """Fonction permettant de retourner la distance entre un point et la spline
+
+        Parameters
+        ----------
+        pk: `int`
+            Point kilométrique au niveau duquel la ligne intercepterait le point
+        point: `list`
+            Longitude et Latitude du point à vérifier"""
+        # Vérifie en premier lieu que le pk est bien sur la spline, sinon retourne une valeur au dessus de l'erreur
+        if pk < self.pk_debut or pk > self.pk_fin:
+            return DISTANCE_ERROR + 1
+
+        # Sinon vérifie s'il est en ligne droite, en courbe ou si la donnée n'existe pas.
+        associated_curve = self.curves.loc[(self.curves[DB.PK + DB.DEBUT] <= pk) & (self.curves[DB.PK + DB.FIN]) < pk]
+        if associated_curve.empty:
+            # Dans le cas où la spline n'a pas d'informations entrées, retourne une distance invalidante
+            return DISTANCE_ERROR + 1
+        else:
+            if associated_curve.iloc[0].SENS.str.upper() == "ALIGNEMENT":
+                Spline.get_distance_to_line()
+
+    @staticmethod
+    def get_distance_to_line(line, point):
+        """Fonction permettant de retourner la distance minimale entre une ligne et un point
+
+        Parameters
+        ----------
+        line: `pd.Series`
+            Information sur la section de spline
+        point: `list`
+            coordonées x et y du point à localiser
+
+        Returns
+        -------
+        distance: `float`
+            distance minimale entre le point et la droite
+        """
+        # ax1 + b = y1      =>      a = (x1-x2)/(y1+y2)
+        # ax2 + b = y2      =>      b = y1-x1*(x1-x2)/(y1+y2)
+        x1, y1 = Spline.geo_to_position(line[DB.GEO_LO + DB.DEBUT], line[DB.GEO_LA + DB.DEBUT])
+        x2, y2 = Spline.geo_to_position(line[DB.GEO_LO + DB.FIN], line[DB.GEO_LA + DB.FIN])
+        a = (x1 - x2) / (y1 + y2)
+        b = y1 - x1 * a
+
+        # d =  |y - ax - b|/sqrt(1 + a²)
+        return abs(point[1] - a * point[1] - b) / ((1 + a ** 2) ** 0.5)
+
+    @staticmethod
+    def get_distance_to_curve(line, point):
+        """Fonction permettant de retourner la distance minimale entre une ligne et un point
+
+        Parameters
+        ----------
+        line: `pd.Series`
+            Information sur la section de spline
+        point: `list`
+            coordonées x et y du point à localiser
+
+        Returns
+        -------
+        distance: `float`
+            distance minimale entre le point et la droite
+        """
+        return DISTANCE_ERROR + 1
+        #TODO : faire la fonction qui retourne la distance entre une courbe et un point
+
+    @staticmethod
+    def geo_to_position(longitude, latitude):
+        """A partir de la longitude et de la latitude retourne la position en m par rapport à 0°, 0°
+
+        Parameters
+        ----------
+        longitude: `float`
+            Longitude du point
+        latitude: `float`
+            Latitude du point
+
+        Returns
+        -------
+        x: `float`
+            Position x par rapport au point 0° 0°
+        y: `float`
+            Position y par rapport au point 0° 0°
+        """
+        return longitude, latitude
+        #TODO : faire l'algorithme de conversion longitude/latitude -> x/y (en m)
+
+    def get_geo_from_pk(self, pk):
+        return 0, 0
+        #TODO : fonction permettant de retourner les coordonées géographiques à partir d'un point kilométrique
+        # Possibilité de le divier en 2 fonctions comme geo_to_position()
+
+
+    def get_more_restrictive(self, pk_begin, pk_end):
+        """Fonction permettant de retourner le profil le plus contraignant entre les pk_begin et pk_end.
+        Si pk_begin < pk_end -> on considèrera que la voie est prise en direction opposée).
+
+        Parameters
+        ----------
+        pk_begin: `int`
+            Le pk de début de la vérification (le profil retourné se trouvera après ce PK)
+        pk_end: `int`
+            Le pk de fin de la vérification (le profil retourné se trouve avant ce PK)
+
+        Returns
+        -------
+        curve_radius: `float`
+            Le rayon de courbure du profile le plus contraignant
+        slope: `float`
+            La pente/rampe du profil le plus contraignant
+        """
+        return 0, 0
+        # TODO: faire la fonction permettant de retourner le profil le plus restrictif entre deux pk
