fe9bcf9126d1c1831b53ef1ae711fa427c4da4c4
MathieuDenglos <matdenglos@gmail.com>
2022-01-05 16:39:48 +0100

    add init function for coaches class

--- src/initialisation/signals/page_rb/pagerb2/complex_data.py
+++ src/initialisation/signals/page_rb/pagerb2/complex_data.py
@@ -54,6 +54,58 @@ class Coaches:
     Cfull = 0
     multiply_mass_full = False
 
+    def __init__(self, mission_type, position_type, position_index, levels, doors, Mtare, Mfull, length,
+                 ABCempty, multiply_mass_empty, ABCfull, multiply_mass_full):
+        """Fonction permettant d'initialiser une voiture
+
+        Parameters
+        ----------
+        mission_type: `MissionType`
+            Type de mission associé à la voiture
+        Position_type: `Position`
+            Permet d'indiquer la forme de la voiture (avant : pupitre à gauche ; arrière : pupitre à droite)
+        position_index: `int`
+            Position de la voiture dans le train
+        levels: `int`
+            Nombre de niveau dans la voiture (0 si fret, 1 ou 2 si passagers)
+        doors: `int`
+            Nombre de portes dans la voiture (0 si fret)
+        Mtare: `float`
+            Masse à vide (en tonnes)
+        Mfull: `float`
+            Masse à charge maximale (en tonnes)
+        length: `float`
+            Longueur de la voiture (en mètres)
+        ABCempty: `list`
+            facteur ABC lorsque le train n'est pas chargé [A (en kN) ; B (en kN/(km/h)) ; C (en kN/(km/h)²)]
+        multiply_mass_empty: `bool`
+            Indique si les facteurs sans charge doivent être multipliés par la masse du train
+        ABCfull: `list`
+            facteur ABC lorsque le train est chargé [A (en kN) ; B (en kN/(km/h)) ; C (en kN/(km/h)²)]
+        multiply_mass_empty: `bool`
+            Indique si les facteurs avec charge doivent être multipliés par la masse du train
+        """
+        self.mission_type = mission_type
+        self.position_type = position_type
+        self.position_index = position_index
+        self.levels = levels
+        self.doors = doors
+        self.Mtare = Mtare
+        self.Mfull = Mfull
+        self.length = length
+
+        if isinstance(ABCempty, list):
+            self.Aempty = 0 if len(ABCempty) <= 0 else ABCempty[0]
+            self.Bempty = 0 if len(ABCempty) <= 1 else ABCempty[1]
+            self.Cempty = 0 if len(ABCempty) <= 2 else ABCempty[2]
+        self.multiply_mass_empty = multiply_mass_empty
+
+        if isinstance(ABCfull, list):
+            self.Afull = 0 if len(ABCfull) <= 0 else ABCfull[0]
+            self.Bfull = 0 if len(ABCfull) <= 1 else ABCfull[1]
+            self.Cfull = 0 if len(ABCfull) <= 2 else ABCfull[2]
+        self.multiply_mass_full = multiply_mass_full
+
 
 class Bogie:
     """classe contenant toutes les informations sur le bogie"""
