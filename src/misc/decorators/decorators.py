c2928dfa010e9b35e5cc5e569db4be2ecde6c211
MathieuDenglos <matdenglos@gmail.com>
2022-01-31 23:26:21 +0100

    refactor all comments of decorators module

--- src/misc/decorators/decorators.py
+++ src/misc/decorators/decorators.py
@@ -10,28 +10,28 @@ sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
 
 
-# Appel : #decorators.QtSignal(log_level=log.Level.DEBUG, end_process=False)
+# Appel : @decorators.QtSignal(log_level=log.Level.DEBUG, end_process=False)
 def QtSignal(log_level=log.Level.ERROR, end_process=False):
-    """décorateur permettant de récupérer les exceptions dans les signaux
+    """décorateur permettant de récupérer les exceptions dans les signaux (ne pouvant pas être récupéré par le main)
 
     Parameters
     ----------
     log_level: `log.Level`
-        Le niveau du message de registre
+        Le niveau du message de registre dans le cas où le signal jette une exception
     end_process: `bool`
         Est ce que l'erreur doit générer une sortie de l'application
     """
     def signal_decorator(signal):
         @wraps(signal)
         def signal_wraper(*args):
-            # Appelle la fonction normalement et récupère une potentielle erreur
             try:
+                # Appelle la fonction normalement et récupère une potentielle erreur
                 signal(*args)
             except Exception as error:
-                # Récupère le nom de la fonction pour plus d'indication et
+                # Récupère le nom de la fonction pour plus d'indication et laisse un message de registre
                 function_name = str(signal)[10:].split(" at ")[0]
                 log.log(log_level=log_level,
-                        message=f"Exception jeté dans la fonction : {function_name}().",
+                        message=f"Exception jeté dans le signal : {function_name}().",
                         exception=error)
                 if end_process:
                     exit(-1)
