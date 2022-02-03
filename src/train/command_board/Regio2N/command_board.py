9990196e2ade166d55c910802b5785aa98fe4bd6
MathieuDenglos <matdenglos@gmail.com>
2022-02-03 21:57:28 +0100

    add main function and imports for Regio2N command board

--- src/train/command_board/Regio2N/command_board.py
+++ src/train/command_board/Regio2N/command_board.py
@@ -2,16 +2,22 @@
 import sys
 import os
 import time
+import threading
 
 
 # Librairie de commande pupitre
 import pyfirmata.pyfirmata as pyfirmata
 
 
+# Librairies graphiques
+from PyQt5.QtWidgets import QApplication
+
+
 # Librairies SARDINE
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
+import src.misc.settings_dictionary.settings as sd
 from src.train.command_board.control import Actions, Control
 
 
@@ -22,12 +28,31 @@ class CommandBoard(Control):
 
 def main():
     # Lance un fichier de registre
-    log.initialise(log_level=log.Level.DEBUG, save=True)
+    log.initialise(log_level=log.Level.DEBUG, save=False)
     log.info(f"Lancement des essais du pupitre léger.\n")
 
-    # TODO : initialiser une instance de la base de donnée train et appeler son constructeur afin de tester le pupitre avec différents matériels roulants
+    # Crée une instance d'une application  et d'une base de données
+    application = QApplication(sys.argv)
+    traindatabase = None        # TODO : remplacer par la vrai base de données
 
-    # TODO : appeler la fonction d'initialisation du pupitre
+    # Génère une instance du pupitre et la lance
+    settings = sd.SettingsDictionary()
+    settings.open(f"{PROJECT_DIR}\\src\\train\\command_board\\Regio2N\\Regio2N.board")
+    command_board = CommandBoard(settings, application)
+    command_board.run()
 
-    # TODO : lancer le pupitre
+    update_thread = threading.Thread(target=update, args=(command_board, traindatabase))
+    update_thread.start()
+    update_thread.join()
+    exit(0)
 
+
+def update(command_board, train_database):
+    i = 0
+    DELAY = 0.25
+    while i < 100:
+        command_board.update(train_database)
+        time.sleep(DELAY)
+
+if __name__ == "__main__":
+    main()
