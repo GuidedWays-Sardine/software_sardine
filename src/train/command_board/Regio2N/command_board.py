3bc38933f67a4decff58c90ecd750c7ad5e522b7
MathieuDenglos <matdenglos@gmail.com>
2022-02-06 23:43:27 +0100

    add Regio2N command board initialisation

--- src/train/command_board/Regio2N/command_board.py
+++ src/train/command_board/Regio2N/command_board.py
@@ -5,10 +5,6 @@ import time
 import threading
 
 
-# Librairie de commande pupitre
-import pyfirmata.pyfirmata as pyfirmata
-
-
 # Librairies graphiques
 from PyQt5.QtWidgets import QApplication
 
@@ -18,12 +14,12 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
 import src.misc.settings_dictionary.settings as sd
-from src.train.command_board.control import Actions, Control
+from src.train.command_board.Generic.controls.electronics.control import Control
 
 
 class CommandBoard(Control):
-    pass # TODO : me remplacer par le code
-    # Surcharger les fonctions : initialise_physical_buttons(self) ; get_buttons_state(self) ; read_specific_values(self)
+    """instance du pupitre lourd"""
+    pass
 
 
 def main():
@@ -35,24 +31,35 @@ def main():
     application = QApplication(sys.argv)
     traindatabase = None        # TODO : remplacer par la vrai base de données
 
-    # Génère une instance du pupitre et la lance
+    # Génère les paramètres nécessaires aux essais
     settings = sd.SettingsDictionary()
-    settings.open(f"{PROJECT_DIR}\\src\\train\\command_board\\Regio2N\\Regio2N.board")
-    command_board = CommandBoard(settings, application)
+    settings["sardine simulator.virtual buttons.screen_index"] = 1
+    settings["sardine simulator.virtual buttons.x"] = 1440
+    settings["sardine simulator.virtual buttons.y"] = 0
+    settings["sardine simulator.virtual buttons.w"] = 480
+    settings["sardine simulator.virtual buttons.h"] = 1080
+
+    command_board_settings = sd.SettingsDictionary()
+    command_board_settings.open(f"{PROJECT_DIR}\\src\\train\\command_board\\Regio2N\\Regio2N.board")
+
+    # Génère une instance du pupitre et la lance
+    command_board = CommandBoard(traindatabase, settings, command_board_settings, application)
     command_board.run()
 
-    update_thread = threading.Thread(target=update, args=(command_board, traindatabase))
+    # lance le thread de la mise à jour graphique
+    update_thread = threading.Thread(target=update, args=(command_board,))
     update_thread.start()
     update_thread.join()
-    exit(0)
 
 
-def update(command_board, train_database):
+def update(command_board):
+    """Fonction d'essai permettant de mettre à jour les entrées du pupitre"""
     i = 0
-    DELAY = 0.25
     while i < 100:
-        command_board.update(train_database)
-        time.sleep(DELAY)
+        # Imprime les entrées lues, met à jour et attends avant de faire la mise à jour suivante
+        command_board.update()
+        time.sleep(0.25)
+
 
 if __name__ == "__main__":
     main()
