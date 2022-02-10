1e335048f22276ffa7dc8bacf35ae53b97d9c479
MathieuDenglos <matdenglos@gmail.com>
2022-02-10 19:28:12 +0100

    change data members

--- src/train/train_database/Systems/systems.py
+++ src/train/train_database/Systems/systems.py
@@ -9,15 +9,16 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
 
 import src.misc.settings_dictionary.settings as sd
+import src.train.train_database.database as tdb
 import src.train.train_database.Systems.traction.traction as traction
 import src.train.train_database.Systems.braking.braking as freinage
-import src.train.train_database.Systems.railcar.railcar as coache
+import src.train.train_database.Systems.railcar.railcar as railcar
 import src.train.train_database.Systems.electric.electric as electric
 
 
 class Systems:
 
-    cars = []
+    railcars = []
     electric = []
     traction = []
     braking = []
