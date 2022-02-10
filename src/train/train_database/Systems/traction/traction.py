aa2d12668873b31b399d27eee2559ca23aa1e809
MathieuDenglos <matdenglos@gmail.com>
2022-02-10 18:48:37 +0100

    reorganise data members

--- src/train/train_database/Systems/traction/traction.py
+++ src/train/train_database/Systems/traction/traction.py
@@ -8,19 +8,21 @@ import os
 # Librairies SARDINE
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src\\")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
+import src.train.train_database.database as tdb
 
 
 class Bogie:
-
-    axle_count = 0
-    axle_power = 0
-    motor_position = []
-    state = []
-    ignitable = []
+    """classe contenant toutes les informations sur le bogie"""
+    # Informations générales
+    position_type = None
+    position_index = -1  # Uniquement pour les bogies centraux! Les bogies extérieurs n'ont pas d'index
+    axles_count = 1
     linked_coaches = []
-    power = []
-    type = None
-    bogie_mass = 0
+
+    # Informations sur la motorisation
+    axles_power = []     # Puissance du moteur, sinon 0 si aucun moteur
+    activated = []      # True si le moteur est allumé, False sinon
+    activable = []      # True si le moteur est activable, False sinon (si panne)
 
     def __init__(self):
         self.axle_count = 0
