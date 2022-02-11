26de68e4f23f90bb00a9dfc3116bfcdd29764c2a
MathieuDenglos <matdenglos@gmail.com>
2022-02-11 20:51:39 +0100

    add all elements of braking systems and structure for initialisation

--- src/train/train_database/Systems/braking/braking.py
+++ src/train/train_database/Systems/braking/braking.py
@@ -6,14 +6,28 @@ import os
 # Librairies SARDINE
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
+import src.misc.settings_dictionary.settings as sd
 
-class Freinage:
-    # Define attributes
 
-    type = 0
-    Nbr_frein = 0
+class Braking:
+    # Informations sur certains types de freinages
+    regenerative_activated = False
+    dynamic_activated = False
 
-    def __init__(self):
-        self.type = None
-        self.Nbr_frein = None
+    # Informations sur les systèmes de freinages
+    pad_brakes = None
+    disk_brakes = None
+    magnetic_brakes = None
+    foucault_brakes = None
 
+    def __init__(self, train_data):
+        """Fonction permettant d'initialiser les systèmes de freinages"""
+        # Récupère les valeurs pour le freinage rhéostatique et par régénération
+        self.regenerative_activated = train_data.get_value("regenerative", False)
+        self.dynamic_activated = train_data.get_value("dynamic", False)
+
+        # Initialise des listes vides pour chacuns des systèmes de freinage
+        self.pad_brakes = []
+        self.disk_brakes = []
+        self.magnetic_brakes = []
+        self.foucault_brakes = []
