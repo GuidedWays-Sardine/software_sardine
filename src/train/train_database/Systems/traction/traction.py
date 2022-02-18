0bad2e02f699281870894a8a81b0ad3e3ec447e5
MathieuDenglos <matdenglos@gmail.com>
2022-02-18 16:59:04 +0100

    revamped traction system

--- src/train/train_database/Systems/traction/traction.py
+++ src/train/train_database/Systems/traction/traction.py
@@ -1,139 +1,97 @@
-# Librairies par défaut python
-# Librairies par défaut python
+# Librairies par défaut
 import sys
 import os
-
+from typing import Union
 
 
 # Librairies SARDINE
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src\\")[0]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
 sys.path.append(os.path.dirname(PROJECT_DIR))
+import src.misc.log.log as log
 import src.train.train_database.database as tdb
+from src.train.train_database.Systems.traction.bogie.bogie import Bogie
 
 
-class Bogie:
-    """classe contenant toutes les informations sur le bogie"""
-    # Informations générales
-    position_type = None
-    position_index = -1  # Uniquement pour les bogies centraux! Les bogies extérieurs n'ont pas d'index
-    axles_count = 1
-    linked_railcars = None   # [car_index] ou [car_index, car_index - 1] (les deux index doivent être concurents)
+class Traction:
+    """classe stockant tous tous les éléments nécessaires à la traction"""
+    # Stoque la liste des bogies
+    bogies = None
 
-    # Informations sur la motorisation
-    axles_power = None    # [Pmoteur1, ..., PmoteurN] -> Puissance du moteur, sinon 0 si aucun moteur
-    activated = None      # [bool, ...] -> True si le moteur est allumé, False sinon
-    activable = None      # [bool, ...] -> True si le moteur est activable, False sinon (si panne)
+    def __init__(self):
+        """Fonction permettant d'initialiser une liste de bogies vide (à remplir ensuite)"""
+        self.bogies = []
 
-    def __init__(self, position_type, position_index, linked_railcars, axles_count, motorized_axles, axles_power):
-        """Fonction permettant d'initialiser un bogie
+    def get_bogies(self, position_index, position_type=None):
+        """Fonction permettant d'avoir un bogie avec les index indiqués
 
         Parameters
         ----------
-        position_type: `Union[tdb.Position, NoneType]`
-            La position du bogie sur la voiture (Position.FRONT ; Position.MIDDLE ; Position.BACK).
-            None si le bogie est articulé (la position sera déduit des index des voitures)
-        position_index: `int`
-            L'index de la position si le bogie est central (sinon -1)
-        linked_railcars: `Union[int, list]`
-            Liste des voitures auxquelles le bogie est connecté
-        axles_count: `int`
-            nombre d'essieux
-        motorized_axles: `Union[list, int, NoneType]`
-            list[bool] -> liste de la position des essieux moteurs (doit être de taille axles_count)
-            int -> nombre d'essieux moteurs (doit être inférieur à axles_count)
-            NoneType -> axles_power au format list[float] auquel cas cet argument n'est pas nécessaire
-        axles_power: `Union[list, float]`
-            list[float] -> liste de la puissance de chacun des moteurs
-            float -> puissance des moteurs (tous les essieux motorisés auront cette puissance)
+        position_index: `Union[int, list]`
+            Les voitures auxquelles le bogie est connecté (int ou liste de int) doit aller de 0 à Nvoitures - 1
+        position_type: `Position`
+            La position des bogies dans la voiture (Position.FRONT ; Position.MIDDLE ; Position.BACK) si cela est vital
+
+        Returns
+        -------
+        bogies_list: `list`
+            liste des bogies suivant les conditions envoyés
         """
-        self.set_general_values(position_type, position_index, linked_railcars, axles_count, motorized_axles, axles_power)
+        # Transforme l'index de la position du bogie en liste d'index si l'index envoyé est un entier
+        if isinstance(position_index, int):
+            position_index = [position_index]
 
-    def set_general_values(self, position_type, position_index, linked_railcars, axles_count, motorized_axles, axles_power):
-        """Fonction permettant de changer les valeurs d'un bogie
-
-        Parameters
-        ----------
-        position_type: `Union[tdb.Position, NoneType]`
-            La position du bogie sur la voiture (Position.FRONT ; Position.MIDDLE ; Position.BACK).
-            None si le bogie est articulé (la position sera déduit des index des voitures)
-        position_index: `int`
-            L'index de la position si le bogie est central (sinon -1)
-        linked_railcars: `Union[int, list]`
-            Liste des voitures auxquelles le bogie est connecté
-        axles_count: `int`
-            nombre d'essieux
-        motorized_axles: `Union[list, int, NoneType]`
-            list[bool] -> liste de la position des essieux moteurs (doit être de taille axles_count)
-            int -> nombre d'essieux moteurs (doit être inférieur à axles_count)
-            NoneType -> axles_power au format list[float] auquel cas cet argument n'est pas nécessaire
-        axles_power: `Union[list, float]`
-            list[float] -> liste de la puissance de chacun des moteurs
-            float -> puissance des moteurs (tous les essieux motorisés auront cette puissance)
-        """
-        # initialise les caractéristiques générales du bogie
-        if isinstance(linked_railcars, list):
-            self.linked_railcars = linked_railcars
-        elif isinstance(linked_railcars, int):
-            self.linked_railcars = [linked_railcars]
-        self.position_index = position_index
-        self.position_type = position_type if len(self.linked_railcars) == 1 else None
-        self.axles_count = axles_count if axles_count >= 1 else 1
-
-        # Initialise la puissance des essieux moteurs
-        # Cas 1 : la puissance des essieux moteurs est une liste de float
-        if isinstance(axles_power, list):
-            # S'assure juste qu'il y a autant d'éléments qu'il y a d'essieux sinon ralonge/raccourcis la liste
-            self.axles_power = [axles_power[i] if i < len(axles_power) else 0.0 for i in range(self.axles_count)]
-        # Cas 2 : la puissance des essieux moteurs est un float
-        elif isinstance(axles_power, float) or isinstance(axles_power, int):
-            # Cas 2.1 : les essieux moteurs est une liste
-            if isinstance(motorized_axles, list):
-                # Met la puissance à axles_power pour chacun des essieux moteurs et ralong/raccourcis la liste au besoin
-                self.axles_power = [bool(motorized_axles[i]) * axles_power if i < len(motorized_axles) else 0.0 for i in range(self.axles_count)]
-            # Cas 2.2 : les essieux moteurs est un int (nombre d'essieux moteurs
-            elif isinstance(motorized_axles, int) or isinstance(motorized_axles, float):
-                # Met la puissance pour les motorized_axles premiers essieux, sinon la met à 0
-                self.axles_power = [axles_power * (i < motorized_axles) for i in range(self.axles_count)]
-            # Cas 2.3 : les essieux moteurs ne sont ni une liste de bool, ni un int
-            else:
-                # Rend l'essieu porteur (puissances moteurs de 0.0) pour éviter l'exception de lecture hors du tableau
-                self.axles_power = [0.0] * self.axles_count
-        # Cas 3 : la puissance des essieux moteurs n'es ni une liste de float ni un float
+        # Cas où le/les bogies recherchés se trouvent sur une seule voiture
+        if len(position_index) == 1:
+            # Le bogie est sur la voiture dans le cas où :
+            # - l'index envoyé est dans la liste et qu'aucune position n'a été donnée
+            # - l'index envoyé est dans la liste et que le type de position correspond à celui du bogie
+            # - le bogie avant est cherché et l'index de la voiture actuelle et précédente sont dans la liste
+            # - le bogie arrière est cherché et l'index de la voiture actuelle et suivant sont dans la liste
+            # - le bogie arrière (ou non spécifié) est cherche et que le bogie avant de la voiture suivant est articulée
+            return [b for b in self.bogies
+                    if ((position_index[0] in b.linked_railcars and (position_type is None or b.position_type == position_type))
+                        or (position_type == tdb.Position.FRONT and position_index[0] in b.linked_railcars and (position_index[0] - 1) in b.linked_railcars)
+                        or (position_type == tdb.Position.BACK and position_index[0] in b.linked_railcars and (position_index[0] + 1) in b.linked_railcars))]
+        # Cas où le bogie recherché se trouve sur deux voiture (bogie jacobien, rame articulée)
+        elif len(position_index) == 2:
+            # Retourne le bogie qui appartient aux deux voitures s'il existe (sinon retourne liste vide)
+            return [b for b in self.bogies if all(p_i in b.linked_railcars for p_i in position_index)
+                    and (position_type is None or b.position_type == position_type)]
+        # Cas où trop d'index ou aucun index n'ont été donnés (retourne une liste vide)
         else:
-            # Rend l'essieu porteur (puissances moteurs de 0.0) pour éviter l'exception de lecture hors du tableau
-            self.axles_power = [0.0] * self.axles_count
+            return []
 
-        # Mets les essieux comme éteints et rends tous les essieux moteurs activables
-        self.activated = [False] * self.axles_count
-        self.activable = [bool(self.axles_power[i]) for i in range(self.axles_count)]  # bool() sort False que pour 0.0
+    def add_bogie(self, bogie):
+        """Fonction permettant de rajouter un bogie à la liste de bogies du système de traction
 
-    def get_general_values(self):
-        """Fonction permettant de retourner toutes les valeurs de paramétrages bogie
-
-        Returns
-        -------
-        qml_values: `list`
-            (position_type, position_index, [linked_railcars], axles_count, axles_power)
+        Parameters
+        ----------
+        bogie: `Bogie`
+            bogie à rajouter dans la liste de bogies
         """
-        return (self.position_type, self.position_index, self.linked_railcars, self.axles_count, self.axles_power)
+        # Ajoute le bogie si c'est bien un bogie sinon laisse un message de debug
+        if isinstance(bogie, Bogie):
+            self.bogies.append(bogie)
+        else:
+            log.debug(f"bogie envoyé de mauvais type ({type(bogie)} au lieu de Bogie).")
 
-    def get_motorized_axles_count(self):
-        """Fonction retournant le nombre d'essieux moteurs
+    def remove_bogie(self, bogie):
+        """fonction permettant d'enlever un bogie de la liste des bogies (par élément ou par bogie)
 
-        Returns
-        -------
-        Motorized_axles_count: `int`
-            Nombre d'essieux motorisés du bogie
+        Parameters
+        ----------
+        bogie: `Union(Bogie, int)`
+            Bogie -> bogie à enlever (à récupérer avec la méthodes get_bogies()[...]
+            int -> position dans la liste de bogies du bogie (Attention! Les bogies ne sont pas forcément dans l'ordre).
         """
-        # nombre d'essieux totaux - nombre d'essieux porteurs
-        return self.axles_count - self.axles_power.count(0)
-
-    def is_jacob_bogie(self):
-        """Fonction retournant l'indication sur si l'essieu est moteur ou non
-
-        Returns
-        -------
-        is_jacob_bogie: `True`
-            retourne vrai si l'essieu est articulé, sinon faux
-        """
-        return len(self.linked_railcars) == 2
+        # Enlève le bogie envoyé en s'assurant qu'il existe bien encore dans la liste
+        if isinstance(bogie, Bogie) and bogie in self.bogies:
+            self.bogies.remove(bogie)
+        elif isinstance(bogie, int) and -len(self.bogies) <= bogie < len(self.bogies):
+            self.bogies.pop(bogie)
+        elif isinstance(bogie, Bogie) and bogie not in self.bogies:
+            log.debug(f"Impossible de supprimer le bogie {bogie} qui n'est pas dans la liste.")
+        elif isinstance(bogie, int) and (bogie >= len(self.bogies) or bogie < -len(self.bogies)):
+            log.debug(f"Impossible de supprimer le bogie {bogie}. Le train a seulement {len(self.bogies)} bogies.")
+        else:
+            log.debug(f"{type(bogie)} n'est pas un bogie valide. Il doit être de type Bogie ou int.")
