3cb1980f172f18e96ea25a7e4bec10878a20109e
MathieuDenglos <matdenglos@gmail.com>
2022-03-26 18:52:33 +0100

    move CommandBoardComponent decorator at the begining of the file

--- src/misc/decorators/decorators.py
+++ src/misc/decorators/decorators.py
@@ -17,6 +17,24 @@ sys.path.append(os.path.dirname(PROJECT_DIR))
 import src.misc.log.log as log
 
 
+# Appel : @decorators.CommandBoardComponent
+def CommandBoardComponent():
+    """décorateur permettant de récupérer les exceptions lors de l'initialision d'une des sous-classe pour le pupitre"""
+    def signal_decorator(init):
+        @wraps(init)
+        def signal_wraper(*args):
+            try:
+                # Appelle la fonction normalement et récupère une potentielle erreur
+                init(*args)
+            except Exception as error:
+                # Laisse un message en niveau log.WARNING pour indiquer que le composant n'a pas su être chargé
+                log.warning("Un des composant pour le pupitre n'a pas pu être chargé correctement : \n" +
+                            f"\tErreur de type : {type(error)}\n\tAvec comme message d'erreur : {error.args}\n\t")
+
+        return signal_wraper
+    return signal_decorator
+
+
 # Appel : @decorators.QtSignal(log_level=log.Level.DEBUG, end_process=False)
 def QtSignal(log_level=log.Level.ERROR, end_process=False):
     """décorateur permettant de récupérer les exceptions dans les signaux (ne pouvant pas être récupéré par le main)
@@ -42,24 +60,6 @@ def QtSignal(log_level=log.Level.ERROR, end_process=False):
                         exception=error)
                 if end_process:
                     exit(-1)
-
-        return signal_wraper
-    return signal_decorator
-
-
-# Appel : @decorators.CommandBoardComponent
-def CommandBoardComponent():
-    """décorateur permettant de récupérer les exceptions lors de l'initialision d'une des sous-classe pour le pupitre"""
-    def signal_decorator(init):
-        @wraps(init)
-        def signal_wraper(*args):
-            try:
-                # Appelle la fonction normalement et récupère une potentielle erreur
-                init(*args)
-            except Exception as error:
-                # Laisse un message en niveau log.WARNING pour indiquer que le composant n'a pas su être chargé
-                log.warning("Un des composant pour le pupitre n'a pas pu être chargé correctement : \n" +
-                            f"\tErreur de type : {type(error)}\n\tAvec comme message d'erreur : {error.args}\n\t")
 
         return signal_wraper
     return signal_decorator
