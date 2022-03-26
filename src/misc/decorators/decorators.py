57e2e382d685ba39dee084b9a0ce6753196e01d2
MathieuDenglos <matdenglos@gmail.com>
2022-03-26 19:06:51 +0100

    fix CommandBoardComponent not returning the initialised component when no issues detected

--- src/misc/decorators/decorators.py
+++ src/misc/decorators/decorators.py
@@ -25,7 +25,7 @@ def CommandBoardComponent():
         def signal_wraper(*args):
             try:
                 # Appelle la fonction normalement et récupère une potentielle erreur
-                init(*args)
+                return init(*args)
             except Exception as error:
                 # Laisse un message en niveau log.WARNING pour indiquer que le composant n'a pas su être chargé
                 log.warning("Un des composant pour le pupitre n'a pas pu être chargé correctement : \n" +
