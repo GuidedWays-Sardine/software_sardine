91345e7973410677b8908f14bf900dd91d86731b
MathieuDenglos <matdenglos@gmail.com>
2022-04-16 17:08:11 +0200

    add __init__.py file to decorators module for better use

---
+++ src/misc/decorators/__init__.py
@@ -0,0 +1,3 @@
+# ENHANCE : Ne pas oublier de rajouter l'import du décorateur ici, sinon il ne pourra pas être appelé
+from .decorators import CommandBoardComponent
+from .decorators import QtSignal, UIupdate
