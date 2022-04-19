9f2761d063459d080c2a0ba331fb071eff8c7211
MathieuDenglos <matdenglos@gmail.com>
2022-04-19 10:17:58 +0200

    revamp log module __init__.py file to fit with modifications in module

--- src/misc/log/__init__.py
+++ src/misc/log/__init__.py
@@ -1,3 +1,5 @@
-from .log import Level, initialise, stop
-from .log import log, debug, info, warning, error, critical
+from .log_levels import Level
+from .log import initialise, log, debug, info, warning, error, critical
 from .log import change_log_level, change_log_prefix, add_empty_lines
+from .log_window import log_window_windowed, log_window_frameless, log_window_visibility
+from .log_window import get_log_window_geometry, set_log_window_geometry
