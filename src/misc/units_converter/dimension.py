2e98e61cb4fd5b2953965a8ac4b4b2a471c51c05
MathieuDenglos <matdenglos@gmail.com>
2022-04-27 22:27:51 +0200

    made all elements of dimension private

--- src/misc/units_converter/dimension.py
+++ src/misc/units_converter/dimension.py
@@ -16,22 +16,22 @@ class Dimension:
     """Classe permettant de définir chaque unité, dépendament de ses dimensions et des units SI"""
 
     # Nom de l'unité (pour affichage)
-    name = ""
+    __name = ""
 
     # Dimensions du système international (en SettingsDictionary pour les rendre insensible au majuscules et minuscules)
-    dimensions = sd.SettingsDictionary({"length": 0.0,  # meter (m)     [L]
-                                        "mass": 0.0,  # kilogram (kg) [M]
-                                        "time": 0.0,  # second (s)    [T]
-                                        "current": 0.0,  # Amps (A)      [I]
-                                        "temperature": 0.0,  # Kelvin (K)    [Θ]
-                                        "light_intensity": 0.0,  # candela (cd)  [N]
-                                        "matter_quantity": 0.0})  # mol (mol)     [J]
+    __dimensions = sd.SettingsDictionary({"length": 0.0,  # meter (m)     [L]
+                                          "mass": 0.0,  # kilogram (kg) [M]
+                                          "time": 0.0,  # second (s)    [T]
+                                          "current": 0.0,  # Amps (A)      [I]
+                                          "temperature": 0.0,  # Kelvin (K)    [Θ]
+                                          "light_intensity": 0.0,  # candela (cd)  [N]
+                                          "matter_quantity": 0.0})  # mol (mol)     [J]
 
     # Facteur (comparé au système international)
-    factor = 0
+    __factor = 0
 
     # décallage (comparé au système international)
-    offset = 0
+    __offset = 0
 
     @functools.cache
     def __init__(self, name, factor=1.0, offset=0.0,
@@ -75,19 +75,19 @@ class Dimension:
         # S'assure que la température n'a pas une dimension différent de 1, 0, -1
         if temperature not in (-1, 0, 1):
             raise ValueError("La dimensions de température ne peut pas être différente de -1 ; 0 ; 1. " +
-                             f"[Θ1] = {self.dimensions['temperature']} ({self.name})")
+                             f"[Θ1] = {self.__dimensions['temperature']} ({self.__name})")
 
         # Sinon initialise chacun des membres
-        self.name = name.replace("x", "*").replace("**", "^").replace("²", "^2").replace(",", ".")
-        self.factor = factor
-        self.offset = offset
-        self.dimensions = {"length": length,
-                           "mass": mass,
-                           "time": time,
-                           "current": current,
-                           "temperature": temperature,
-                           "light_intensity": light_intensity,
-                           "matter_quantity": matter_quantity}
+        self.__name = name.replace("x", "*").replace("**", "^").replace("²", "^2").replace(",", ".")
+        self.__factor = factor
+        self.__offset = offset
+        self.__dimensions = {"length": length,
+                             "mass": mass,
+                             "time": time,
+                             "current": current,
+                             "temperature": temperature,
+                             "light_intensity": light_intensity,
+                             "matter_quantity": matter_quantity}
 
     def __hash__(self):
         """Retourne la référence vers la dimension."""
@@ -102,7 +102,7 @@ class Dimension:
             Retourne vrai si la dimension des deux unités est identique
         """
         return isinstance(other, Dimension) and \
-            all(isclose(self.dimensions[key], other.dimensions[key]) for key in list(self.dimensions.keys()))
+            all(isclose(self.__dimensions[key], other.__dimensions[key]) for key in list(self.__dimensions.keys()))
 
     def __rmul__(self, other):
         """Opérateur de multiplication : * (inversé)
@@ -149,28 +149,28 @@ class Dimension:
         """
         # Si la valeur est un nombre, retourne la conversion de ce nombre de l'unité SI à l'unité envoyée
         if isinstance(other, (float, int)):
-            return (other - self.offset) * self.__pow__(-1).factor
+            return (other - self.__offset) * self.__pow__(-1).__factor
         # On sait que la seul dimension avec des unités à décallage est la température (avec le Celsius et le Farenheit)
         # Si des [Θ] sont divisés par des [Θ]^-1 ou inversement, jette une erreur (les [Θ]^2 n'existent pas)
         elif isinstance(other, Dimension) and \
-                (not (self.dimensions["temperature"] + other.dimensions["temperature"]) in (-1, 0, 1) or
-                 (not isclose(self.offset, 0) and not isclose(other.offset, 0))):
+                (not (self.__dimensions["temperature"] + other.__dimensions["temperature"]) in (-1, 0, 1) or
+                 (not isclose(self.__offset, 0) and not isclose(other.__offset, 0))):
             raise ValueError("La dimensions de température ne peut pas être différente de -1 ; 0 ; 1. " +
-                             f"[Θ1] = {self.dimensions['temperature']} ({self.name}) ; " +
-                             f"[Θ2] = {other.dimensions['temperature']} ({other.name}) -> " +
-                             f"[Θ1]*[Θ2] = {self.dimensions['temperature'] + other.dimensions['temperature']}")
+                             f"[Θ1] = {self.__dimensions['temperature']} ({self.__name}) ; " +
+                             f"[Θ2] = {other.__dimensions['temperature']} ({other.__name}) -> " +
+                             f"[Θ1]*[Θ2] = {self.__dimensions['temperature'] + other.__dimensions['temperature']}")
         # Sinon, les unités sont bonnes, retourne la division des deux unités
         elif isinstance(other, Dimension):
-            return Dimension(name=f"{self.name}*{other.name}",
-                             factor=self.factor * other.factor,
-                             offset=self.offset + other.offset,
-                             length=self.dimensions["length"] + other.dimensions["length"],
-                             mass=self.dimensions["mass"] + other.dimensions["mass"],
-                             time=self.dimensions["time"] + other.dimensions["time"],
-                             current=self.dimensions["current"] + other.dimensions["current"],
-                             temperature=self.dimensions["temperature"] + other.dimensions["temperature"],
-                             light_intensity=self.dimensions["light_intensity"] + other.dimensions["light_intensity"],
-                             matter_quantity=self.dimensions["matter_quantity"] + other.dimensions["matter_quantity"])
+            return Dimension(name=f"{self.__name}*{other.__name}",
+                             factor=self.__factor * other.__factor,
+                             offset=self.__offset + other.__offset,
+                             length=self.__dimensions["length"] + other.__dimensions["length"],
+                             mass=self.__dimensions["mass"] + other.__dimensions["mass"],
+                             time=self.__dimensions["time"] + other.__dimensions["time"],
+                             current=self.__dimensions["current"] + other.__dimensions["current"],
+                             temperature=self.__dimensions["temperature"] + other.__dimensions["temperature"],
+                             light_intensity=self.__dimensions["light_intensity"] + other.__dimensions["light_intensity"],
+                             matter_quantity=self.__dimensions["matter_quantity"] + other.__dimensions["matter_quantity"])
         # Récupère dans le cas où le type n'est pas correct
         else:
             raise TypeError("unsupported operand type(s) for /: 'Dimension' and " +
@@ -196,7 +196,7 @@ class Dimension:
         """
         # Si la valeur est un nombre, retourne la conversion de ce nombre de l'unité envoyé à son équivalent SI
         if isinstance(other, (int, float)):
-            return other * self.factor + self.offset
+            return other * self.__factor + self.__offset
         # Sinon jette une erreur pour indiquer que les types ne sont pas compatibles
         else:
             raise TypeError("unsupported operand type(s) for /: " +
@@ -228,31 +228,31 @@ class Dimension:
         """
         # Vérifie que le nombre dans la division n'est pas 0, sinon jette une erreur
         if isinstance(other, (float, int)) and other == 0:
-            raise ZeroDivisionError(f"division by zero ({self.name}/0)")
+            raise ZeroDivisionError(f"division by zero ({self.__name}/0)")
         # Si la valeur dans la division est un nombre, retourne la multiplication de l'unité par l'inverse de other
         elif isinstance(other, (float, int)):
             return self * (1/other)
         # On sait que la seul dimension avec des unités à décallage est la température (avec le Celsius et le Farenheit)
         # Si des [Θ] sont divisés par des [Θ]^-1 ou inversement, jette une erreur (les [Θ]^2 n'existent pas)
         elif isinstance(other, Dimension) and \
-                not (self.dimensions["temperature"] - other.dimensions["temperature"]) in (-1, 0, 1):
+                not (self.__dimensions["temperature"] - other.__dimensions["temperature"]) in (-1, 0, 1):
             raise ValueError("La dimensions de température ne peut pas être différente de -1 ; 0 ; 1. " +
-                             f"[Θ1] = {self.dimensions['temperature']} ({self.name}) ; " +
-                             f" [Θ2] = {other.dimensions['temperature']} ({other.name}) -> " +
-                             f"[Θ1] / [Θ2] = {self.dimensions['temperature'] - other.dimensions['temperature']}")
+                             f"[Θ1] = {self.__dimensions['temperature']} ({self.__name}) ; " +
+                             f" [Θ2] = {other.__dimensions['temperature']} ({other.__name}) -> " +
+                             f"[Θ1] / [Θ2] = {self.__dimensions['temperature'] - other.__dimensions['temperature']}")
         # Sinon, les unités sont bonnes, retourne la division des deux unités
         elif isinstance(other, Dimension):
-            is_complex = (len(re.findall(r"[/*]", other.name)) > 0)      # Pour savoir si l'unité doit être encadrée
-            return Dimension(name=f"{self.name}/{f'({other.name})' if is_complex else other.name}",
-                             factor=self.factor / other.factor,
-                             offset=(self.offset - other.offset) / other.factor * (not isclose(other.offset, 0)),
-                             length=self.dimensions["length"] - other.dimensions["length"],
-                             mass=self.dimensions["mass"] - other.dimensions["mass"],
-                             time=self.dimensions["time"] - other.dimensions["time"],
-                             current=self.dimensions["current"] - other.dimensions["current"],
-                             temperature=self.dimensions["temperature"] - other.dimensions["temperature"],
-                             light_intensity=self.dimensions["light_intensity"] - other.dimensions["light_intensity"],
-                             matter_quantity=self.dimensions["matter_quantity"] - other.dimensions["matter_quantity"])
+            is_complex = (len(re.findall(r"[/*]", other.__name)) > 0)      # Pour savoir si l'unité doit être encadrée
+            return Dimension(name=f"{self.__name}/{f'({other.__name})' if is_complex else other.__name}",
+                             factor=self.__factor / other.__factor,
+                             offset=(self.__offset - other.__offset) / other.__factor * (not isclose(other.__offset, 0)),
+                             length=self.__dimensions["length"] - other.__dimensions["length"],
+                             mass=self.__dimensions["mass"] - other.__dimensions["mass"],
+                             time=self.__dimensions["time"] - other.__dimensions["time"],
+                             current=self.__dimensions["current"] - other.__dimensions["current"],
+                             temperature=self.__dimensions["temperature"] - other.__dimensions["temperature"],
+                             light_intensity=self.__dimensions["light_intensity"] - other.__dimensions["light_intensity"],
+                             matter_quantity=self.__dimensions["matter_quantity"] - other.__dimensions["matter_quantity"])
         # Récupère dans le cas où le type n'est pas correct
         else:
             raise TypeError("unsupported operand type(s) for /: 'Dimension' and " +
@@ -282,25 +282,25 @@ class Dimension:
         """
         # Si le type de la puissance est un int ou un float
         if isinstance(power, (int, float)):
-            # On sait que la seul dimension avec des unités à décallage est la température (avec le Celsius et le Farenheit)
+            # On sait que la seul dimension avec des unités à décallage est la température (avec Celsius et Farenheit)
             # Si des [Θ] sont divisés par des [Θ]^-1 ou inversement, jette une erreur (les [Θ]^2 n'existent pas)
-            if self.dimensions["temperature"] != 0 and power not in (-1, 0, 1):
+            if self.__dimensions["temperature"] != 0 and power not in (-1, 0, 1):
                 raise ValueError("La dimensions de température ne peut pas être différente de -1 ; 0 ; 1. " +
-                                 f"[Θ1] = {self.dimensions['temperature']} ({self.name}) ; power = {power} -> " +
-                                 f"[Θ1]^power = {self.dimensions['temperature'] * power}")
+                                 f"[Θ1] = {self.__dimensions['temperature']} ({self.__name}) ; power = {power} -> " +
+                                 f"[Θ1]^power = {self.__dimensions['temperature'] * power}")
             else:
                 # Sinon retourne la dimension mis à la puissance envoyée
-                is_complex = (len(re.findall(r"[/*]", self.name)) > 0)
-                return Dimension(name=f"{f'({self.name})' if is_complex else self.name}^{power}",
-                                 factor=self.factor**power,
-                                 offset=self.offset * (power == 1),
-                                 length=self.dimensions["length"] * power,
-                                 mass=self.dimensions["mass"] * power,
-                                 time=self.dimensions["time"] * power,
-                                 current=self.dimensions["current"] * power,
-                                 temperature=self.dimensions["temperature"] * power,
-                                 light_intensity=self.dimensions["light_intensity"] * power,
-                                 matter_quantity=self.dimensions["matter_quantity"] * power)
+                is_complex = (len(re.findall(r"[/*]", self.__name)) > 0)
+                return Dimension(name=f"{f'({self.__name})' if is_complex else self.__name}^{power}",
+                                 factor=self.__factor**power,
+                                 offset=self.__offset * (power == 1),
+                                 length=self.__dimensions["length"] * power,
+                                 mass=self.__dimensions["mass"] * power,
+                                 time=self.__dimensions["time"] * power,
+                                 current=self.__dimensions["current"] * power,
+                                 temperature=self.__dimensions["temperature"] * power,
+                                 light_intensity=self.__dimensions["light_intensity"] * power,
+                                 matter_quantity=self.__dimensions["matter_quantity"] * power)
         else:
             raise TypeError("unsupported operand type(s) for **: 'Dimension' and " +
                             str(type(power)).split("\'")[1] if str(type(power)).count("\'") >= 2 else str(type(power)))
@@ -321,11 +321,11 @@ class Dimension:
                    ("light_intensity", "N"),
                    ("matter_quantity", "J"))
 
-        if not any(self.dimensions[key[0]] for key in symbols if not isclose(self.dimensions[key[0]], 0)):
-            return f"Dimension({self.name}, factor={self.factor}, offset={self.offset}, Dimension = [Dimensionless])"
+        if not any(self.__dimensions[key[0]] for key in symbols if not isclose(self.__dimensions[key[0]], 0)):
+            return f"Dimension({self.__name}, factor={self.__factor}, offset={self.__offset}, Dimension = 1)"
         else:
-            return f"Dimension({self.name}, factor={self.factor}, offset={self.offset}, Dimension = " +\
-                   f"{' '.join([f'[{key[1]}]^{self.dimensions[key[0]]}' for key in symbols if not isclose(self.dimensions[key[0]], 0)])})"
+            return f"Dimension({self.__name}, factor={self.__factor}, offset={self.__offset}, Dimension = " +\
+                   f"{' '.join([f'[{key[1]}]^{self.__dimensions[key[0]]}' for key in symbols if not isclose(self.__dimensions[key[0]], 0)])})"
 
     def __repr__(self):
         """Convertit la dimension en un texte lisible pour l'utilisateur.
@@ -347,7 +347,7 @@ class Dimension:
         offset: `float`
             Décallage à rajouter.
         """
-        return 1/self.factor, - self.offset / self.factor
+        return 1/self.__factor, - self.__offset / self.__factor
 
     def unit_to_si(self):
         """Retourne les coefficients de conversions pour passer de cette unité à l'équivalent SI.
@@ -359,5 +359,4 @@ class Dimension:
         offset: `float`
             Décallage à rajouter.
         """
-        return self.factor, self.offset
-
+        return self.__factor, self.__offset
