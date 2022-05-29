ad77f82db3294049d6d6f238530d0b7c332ed229
MathieuDenglos <matdenglos@gmail.com>
2022-05-29 23:33:06 +0200

    implement logic for virtual keyboard

---
+++ src/misc/virtual_keyboard/keyboard.py
@@ -0,0 +1,267 @@
+# Librairies par défaut
+import functools
+import os
+import sys
+import time
+import pyautogui
+
+
+# Librairies graphiques
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QObject, Qt
+
+
+# Librairies SARDINE
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)).split("src")[0]
+sys.path.append(os.path.dirname(PROJECT_DIR))
+import src.misc.decorators as decorators
+import src.misc.log as log
+from src.misc.virtual_keyboard.mode import KeyboardMode, get_keyboard_from_language
+
+
+# Instance du clavier virtuel
+KEYBOARD = []
+
+
+class VirtualKeyboard:
+    """Partie logique du clavier virtuel"""
+    # Chemin d'accès vers le fichier graphique du clavier virtuel
+    keyboard___window_file_path = PROJECT_DIR + "\\src\\misc\\virtual_keyboard\\Keyboard.qml"
+
+    # Eléments nécessaires au chargement de l'image
+    __engine = None
+    __win = None
+
+    # Valeur pour indiquer si le clavier virtuel est activé (mettre à False si un clavier solide est utilisé)
+    __is_activated = True
+
+    def __init__(self):
+        """Initialise le clavier virtuel (par défaut KeyboardMode.NUMPAD).
+
+        Raises
+        ------
+        FileNotFoundError:
+            Jetée si le fichier QML (graphique) du clavier virtuel n'est pas trouvé ;
+        SyntaxError:
+            Jetée si le fichier QML (graphique) du clavier virtuel contient des erreurs.
+        """
+        initial_time = time.perf_counter()
+        # Cherche pour le fichier QML avec tous les éléments de la fenêtre du clavier virtuel
+        self.__engine = QQmlApplicationEngine()
+        self.__engine.load(self.keyboard___window_file_path)
+
+        # Vérifie si le fichier qml de la fenêtre a bien été ouvert et compris, sinon jête une erreur
+        if not self.__engine.rootObjects() and not os.path.isfile(self.keyboard___window_file_path):
+            raise FileNotFoundError(f"Le fichier .qml du clavier virtuel (virtual_keyboard) n'a pas été trouvée.\n\t" +
+                                    self.keyboard___window_file_path)
+        elif not self.__engine.rootObjects() and os.path.isfile(self.keyboard___window_file_path):
+            raise SyntaxError(f"Le fichier .qml du clavier virtuel (virtual_keyboard) contient des erreurs.\n\t" +
+                              self.keyboard___window_file_path)
+        else:
+            # Récupère la fenêtre et la cache
+            self.__win = self.__engine.rootObjects()[0]
+            self.__win.hide()
+
+            # Indique au clavier virtuel de taper la touche à chaque fois qu'elle est touchée
+            # pyautogui.typewrite prend la clé comme argument. Celle-ci est fourni par l'émission du signal
+            self.__win.key_clicked.connect(pyautogui.typewrite)
+            self.__win.key_pushed.connect(pyautogui.press)
+
+            # Indique le temps de chargement du clavier virtuel
+            log.info(f"Initialisation du clavier virtuel en " +
+                     f"{((time.perf_counter() - initial_time) * 1000):.2f} millisecondes.",
+                     prefix="Chargement clavier virtuel")
+
+    def change_keyboard(self, keyboard_mode) -> None:
+        """Change le mode/la langue du clavier virtuel.
+
+        Parameters
+        ----------
+
+        keyboard_mode: `KeyboardMode`:
+            Mode du nouveau clavier (contenant la liste des touches comme valeur).
+        """
+        if isinstance(keyboard_mode, KeyboardMode):
+            # Change le clavier actif
+            self.__win.setProperty("keyboard", keyboard_mode.value)
+
+            # Change la dimension du clavier virtuel (automatiquement réalisé par le fichier graphique)
+            # Dimensions conseillées : 260*300 en mode numpad ; 900*300 en mode clavier
+            self.__win.setProperty("width", 260 if keyboard_mode.value == [] else 900)
+            self.__win.setProperty("height", 300)
+        else:
+            log.warning(f"{keyboard_mode} n'est pas de type KeyboardMode, changement de langue non pris en compte.,",
+                        prefix="Clavier virtuel")
+
+    def change_skip_list(self, skip_list):
+        """Change la liste des charactères interdits (ceux-ci apparaitront mais ne seront pas sélectionable).
+
+        Parameters
+        ----------
+        skip_list: `list[str], tuple[str]`
+            Liste des charactère interdits.
+        """
+        self.__win.setProperty("skip_list", list(skip_list))
+
+    def move_to(self, x, y):
+        """Bouge la fenêtre aux coordonées (absolus) envoyées
+
+        Parameters
+        x: `int`
+            Coordonées x à partir du coin haut, gauche du premier écran ;
+        y: `int`
+            Coordonées y à partir du coin haut, gauche du premier écran ;
+        """
+        self.__win.setProperty("x", x)
+        self.__win.setProperty("y", y)
+
+    def show(self):
+        """Montre le clavier virtuel"""
+        self.__win.show()
+
+    def hide(self):
+        """Cache le clavier virtuel"""
+        self.__win.hide()
+
+    def is_activated(self, new_state=None):
+        """Retourne (et change) l'état de la variable indiquant si le module du clavier virtuel est activé.
+
+        Parameters
+        ----------
+        new_state: `bool | None`
+            Nouvel état de la variable pour si le module du clavier virtuel est activé (None pour ne pas changer).
+
+        Returns
+        -------
+        current_state: `bool`
+            Etat de la variable après changement.
+        """
+        # Change l'état de la variable si un nouvel état a été envoyé
+        if new_state is not None:
+            self.__is_activated = bool(new_state)
+
+        # Retour ce nouvel état/l'état actuel
+        return self.__is_activated
+
+
+@decorators.UniqueCall
+def initialise_keyboard():
+    """Initialise le clavier virtuel. Si celui-ci ne s'initialise par correctement, il ne pourra pas être utilisé."""
+    # Essaye d'initialiser le clavier virtuel. Si celui-ci contient une erreur, l'indique dans le registre
+    try:
+        KEYBOARD.append(VirtualKeyboard())
+    except Exception as error:
+        log.error("Erreur lors de l'initialisation du clavier virtuel. Le clavier virtuel ne sera pas utilisé.",
+                  exception=error, prefix="Clavier virtuel")
+
+
+def change_skip_list(skip_list=()):
+    """Change la liste des charactères interdits.
+
+    Parameters
+    ----------
+    skip_list: `list[str] | tuple[str]`
+        Liste des charactères interdits.
+    """
+    # Si un clavier n'a pas été initialisé, essaye de l'initialiser
+    if not KEYBOARD:
+        initialise_keyboard()
+
+    # Si un clavier a été initialisé, change la skip_list
+    if KEYBOARD:
+        KEYBOARD[0].change_skip_list(skip_list)
+
+
+def change_mode(language=KeyboardMode.NUMPAD):
+    """Change le mode/la langue du clavier virtuel.
+
+    Parameters
+    ----------
+    language: `KeyboardMode | str`
+        langue ou clavier à utiliser
+    """
+    # Si un clavier n'a pas été initialisé, essaye de l'initialiser
+    if not KEYBOARD:
+        initialise_keyboard()
+
+    # Si une langue a été envoyée, récupère le clavier correspondant et change la langue
+    if KEYBOARD and isinstance(language, str):
+        KEYBOARD[0].change_keyboard(get_keyboard_from_language(language))
+    # Si un clavier a directement été envoyé, le définit
+    elif KEYBOARD and isinstance(language, KeyboardMode):
+        KEYBOARD[0].change_keyboard(language)
+
+
+@decorators.QtSignal(log_level=log.Level.WARNING, end_process=False)
+def show_keyboard(window, widget, language=KeyboardMode.NUMPAD, skip_list=()):
+    """Montre le clavier virtuel.
+
+    Parameters
+    ----------
+    window: `QMainWindow | QWindow | QObject`
+        Fenêtre à laquelle appartient le composant ;
+    widget: `QObject`
+        Objet auquel le clavier sera rattaché (généralement l'objet actif).
+        Doit contenir de préfére,ce les variables : [default_x/y, default_width, default_height, ratio, x/y_offset]
+        sinon les positions réelles du composant seront prises en compte ;
+    language: `str | KeyboardMode | None`
+        str -> langue du clavier à utiliser ;
+        KeyboardMode -> clavier à utiliser ;
+        None -> Le clavier ne sera pas changé et le précédent sera utilisé ;
+    skip_list: `list[str] | tuple[str] | None`
+        Liste des lettres à ne pas considérer.
+    """
+    # Si le clavier virtuel n'a pas été initialisé, essaye de l'initialiser
+    if not KEYBOARD:
+        initialise_keyboard()
+
+    # Si le clavier a été initialisé
+    if KEYBOARD:
+        # Change le clavier actuel et la liste des charactères inteerdits
+        change_mode(language)
+        change_skip_list(skip_list)
+
+        # Position de la fenêtre sur l'écran
+        x = window.x() - ("framePosition" in dir(window)) + ("setPosition" not in dir(window))
+        y = window.geometry().y()
+
+        # Ajoute la position du composant sur la fenêtre (différencie le cas d'un composent personalisé et défaut)
+        if not any(widget.property(key) is None for key in ("default_x", "default_y", "default_width", "default_height", "ratio")):
+            # Dans le cas d'un composant personalisé avec auto redimensionnement
+            x += widget.property("default_x") * widget.property("ratio")
+            y += widget.property("default_y") * widget.property("ratio")
+            width = widget.property("default_width") * widget.property("ratio")
+            height = widget.property("default_height") * widget.property("ratio")
+
+            # Ajoute le décallage dans le cas d'une fenêtre qui doit garder strictement le même ratio (DMI, ...)
+            if widget.property("x_offset") is not None and widget.property("y_offset") is not None:
+                x += widget.property("x_offset")
+                y += widget.property("y_offset")
+        else:
+            # Dans le cas d'un composant par défaut avec les propriétés de base
+            x += widget.property("x")
+            y += widget.property("y")
+            width = widget.property("width")
+            height = widget.property("height")
+
+        # TODO l'algorithme pour trouver la meilleure position de la fenêtre
+
+
+        # Déplace la fenêtre aux meilleurs coordonées inscrites
+        KEYBOARD[0].move_to(x, y + height)
+
+        # Montre le clavier virtuel et redonne le focus au composant édité
+        KEYBOARD[0].show()
+        window.give_focus()
+
+
+@decorators.QtSignal(log_level=log.Level.WARNING, end_process=False)
+def hide_keyboard():
+    """Cache le clavier virtuel."""
+    # Si le clavier n'a pas été initialisé, initialise le (il sera caché)
+    if not KEYBOARD:
+        initialise_keyboard()
+    # Sinon, si le clavier n'est pas en train d'être montré, le cache
+    elif KEYBOARD:
+        KEYBOARD[0].hide()
