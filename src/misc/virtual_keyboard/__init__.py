64d5cf985a4401cabca605aae4eb4d02a790a08b
MathieuDenglos <matdenglos@gmail.com>
2022-05-29 23:33:28 +0200

    add __init__ file to transform virtual keyboard as a module

---
+++ src/misc/virtual_keyboard/__init__.py
@@ -0,0 +1,4 @@
+from .mode import KeyboardMode, get_keyboard_from_language
+from .keyboard import initialise_keyboard
+from .keyboard import show_keyboard, hide_keyboard
+from .keyboard import change_mode, change_skip_list
