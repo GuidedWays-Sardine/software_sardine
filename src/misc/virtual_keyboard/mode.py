563c51075da022ae8f3e24fbf49a3c79a02a6f24
MathieuDenglos <matdenglos@gmail.com>
2022-05-29 23:23:35 +0200

    implement function to translate language to according keyboard

--- src/misc/virtual_keyboard/mode.py
+++ src/misc/virtual_keyboard/mode.py
@@ -25,3 +25,29 @@ class KeyboardMode(Enum):
                 ["aA",  "zZ",  "eE€",  "rR",  "tT",  "yY",  "uU",  "iI",   "oO",  "pP",  "^¨",  "$£¤"],
                 ["qQ",  "sS",  "dD",   "fF",  "gG",  "hH",  "jJ",  "kK",   "lL",  "mM",  "ù%",  "*µ", ""],
                 ["<>",  "wW",  "xX",   "cC",  "vV",  "bB",  "nN",  ",?",   ";.",  ":/",  "!§",  ""]]
+
+
+@cache
+def get_keyboard_from_language(language):
+    """Fonction permettant de retourner la valeur du clavier à partir de sa langue.
+
+    Parameters
+    ----------
+    language: `string`
+        Langue du clavier à récupérer (non sensible aux minuscules et majuscules)
+
+    Returns
+    -------
+    keyboard: `KeyboardMode`
+        Clavier de la langue correspondante (anglais si langue introuvable).
+    """
+    # Convertit tous les charactères particuliers en charactères classiques (é -> e ; ç -> c) et passe en majuscule
+    modified = ''.join(c for c in unicodedata.normalize('NFD', language) if unicodedata.category(c) != 'Mn').upper()
+
+    try:
+        # Essaye de retourner la langue du clavier correspondant à la langue
+        return KeyboardMode[modified]
+    except KeyError:
+        # Si la langue n'existe pas, laisse un message de debug et retourne le clavier anglais
+        log.debug(f"Le clavier \"{language}\" (convertir : \"{modified}\") n'existe pas. Clavier anglais utilisé.")
+        return KeyboardMode.ENGLISH
