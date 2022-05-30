12425e214cece4a75f6181c824857a8301e571c9
MathieuDenglos <matdenglos@gmail.com>
2022-05-31 01:01:15 +0200

    add import for activability change functions

--- src/misc/virtual_keyboard/__init__.py
+++ src/misc/virtual_keyboard/__init__.py
@@ -2,3 +2,4 @@ from .mode import KeyboardMode, get_keyboard_from_language
 from .keyboard import initialise_keyboard
 from .keyboard import show_keyboard, hide_keyboard
 from .keyboard import change_mode, change_skip_list
+from .keyboard import change_keyboard_activability, activate_keyboard, deactivate_keyboard
